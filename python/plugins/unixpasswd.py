8d319bc27329ebde943897f4287925882408aea0
Jeremy Noonan <jeremy@keepersecurity.com>
2015-10-08 16:45:58 -0700

    Updated without unecessary class structure since it will be imported as a unique module namespace

--- python/plugins/unixpasswd.py
+++ python/plugins/unixpasswd.py
@@ -1,18 +1,25 @@
+#  _  __  
+# | |/ /___ ___ _ __  ___ _ _ ®
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|            
+#
+# Keeper Commander 
+# Copyright 2015 Keeper Security Inc.
+# Contact: ops@keepersecurity.com
+#
+
 from subprocess import Popen, PIPE
 
-class UnixPasswd:
-    """Commander Plugin for Unix Passwd Command"""
-    def __init__(self, password=''):
-        self.password = password
+"""Commander Plugin for Unix Passwd Command"""
+def login():
+	return
 
-	def login(self):
-	    return
+def logout():
+	return
 
-	def logout(self):
-	    return
-
-	def rotate(self):
-		proc = Popen(['/usr/bin/sudo', '/usr/bin/passwd', 'keepertest'])
-		proc.communicate('password2')
-		proc.communicate('password3')
-		proc.communicate('password3')
+def rotate():
+	proc = Popen(['/usr/bin/sudo', '/usr/bin/passwd', 'keepertest'])
+	proc.communicate('password2')
+	proc.communicate('password3')
+	proc.communicate('password3')
