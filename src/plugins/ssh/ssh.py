a74552c1fe7e13440af97aecd098b86d301b349c
Jeremy Noonan <jeremy@keepersecurity.com>
2015-11-12 15:22:59 -0800

    KC-37 Addition of an SSH plugin

---
+++ src/plugins/ssh/ssh.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#  _  __  
+# | |/ /___ ___ _ __  ___ _ _ ®
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|            
+#
+# Keeper Commander 
+# Copyright 2015 Keeper Security Inc.
+# Contact: ops@keepersecurity.com
+#
+
+import pexpect
+from pexpect import pxssh
+
+"""Commander Plugin for SSH Command
+   Dependencies: 
+       pip3 install pexpect
+"""
+
+def rotate(record, newpassword):
+    """ Grab any required fields from the record """
+    user = record.login
+    oldpassword = record.password
+
+    result = False
+
+    host = record.get('cmdr:host')
+
+    try:
+        s = pxssh.pxssh()
+        s.login(host, user, oldpassword)
+        s.sendline('passwd')
+        i = s.expect(['[Oo]ld [Pp]assword', '.current.*password', '[Nn]ew [Pp]assword'])
+        if i == 0 or i == 1:
+            s.sendline(oldpassword)
+            s.expect('[Nn]ew [Pp]assword')
+        s.sendline(newpassword)
+        s.expect("Retype New Password:")
+        s.sendline(newpassword)
+        s.prompt()
+
+        pass_result = s.before
+
+        if pass_result == b'\r\n':
+            print('Password changed succesfully')
+            result = True
+        else:
+            print('Password change failed:')
+            print(pass_result)
+
+        s.logout()
+    except exceptions.TIMEOUT as t:
+        print("Timed out waiting for response.")
+    except pxssh.ExceptionPxssh as e:
+        print("Failed to login with ssh.")
+
+    return result
