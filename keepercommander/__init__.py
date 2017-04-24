edc1c49126929929a56bb6d4d368f75ed7095030
KISHORE PEDDETI <kishore@keepersecurity.com>
2017-04-24 14:30:25 -0700

    New commander version updated for PIP3

--- keepercommander/__init__.py
+++ keepercommander/__init__.py
@@ -3,7 +3,7 @@ import click
 
 from keepercommander import cli, display, api
 
-__version__ = '0.6.4'
+__version__ = '0.6.5'
 
 @click.group()
 @click.option('--server', '-s', envvar='KEEPER_SERVER', help='Host address. You can set KEEPER_SERVER environment variable instead.')
