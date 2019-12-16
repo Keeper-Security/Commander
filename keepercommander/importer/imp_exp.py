ed2c97babba33a922180db305745a53ce22a505e
Sergey Kolupaev <skolupaev@keepersecurity.com>
2019-12-16 15:36:09 -0800

    Keepass: keyfile only login

--- keepercommander/importer/imp_exp.py
+++ keepercommander/importer/imp_exp.py
@@ -244,23 +244,27 @@ def _import(params, file_format, filename, **kwargs):
     if records:
         # create folders
         folder_add = prepare_folder_add(params, records)
-        api.execute_batch(params, folder_add)
-        api.sync_down(params)
+        if folder_add:
+            api.execute_batch(params, folder_add)
+            api.sync_down(params)
 
         # create records
         record_adds = prepare_record_add(params, records)
-        api.execute_batch(params, record_adds)
-        api.sync_down(params)
+        if record_adds:
+            api.execute_batch(params, record_adds)
+            api.sync_down(params)
 
         # ensure records are linked to folders
         record_links = prepare_record_link(params, records)
-        api.execute_batch(params, record_links)
-        api.sync_down(params)
+        if record_links:
+            api.execute_batch(params, record_links)
+            api.sync_down(params)
 
         # adjust shared folder permissions
         shared_update = prepare_record_permission(params, records)
-        api.execute_batch(params, shared_update)
-        api.sync_down(params)
+        if shared_update:
+            api.execute_batch(params, shared_update)
+            api.sync_down(params)
 
         # upload attachments
         atts = []
