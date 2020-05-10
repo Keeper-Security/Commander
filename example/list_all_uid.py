45c978e32207d7346ede00fcc1b1051dd118e450
Mkn,Yskz <mkn@yskz.dev>
2020-05-10 00:27:37 +0000

    KeeperLogin class for with statement

--- example/list_all_uid.py
+++ example/list_all_uid.py
@@ -1,50 +1,50 @@
 # Show all UIDs in Vault
-import logging
-logger = logging.getLogger(__name__)
-sHandler = logging.StreamHandler()
-sHandler.setLevel(logging.INFO)
-logger.addHandler(sHandler)
-logfilenode = __file__.rsplit('.')[0]
-handler = logging.FileHandler(f"{logfilenode}.log")
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 import sys
 import os
 sys.path.append("..")
 from keepercommander import api, params
+import getpass
 
+class KeeperLogin(object):
+    ''' Login and sync_down automatically 
+        user-ID is gotten from $KEEPER_USER
+        user password if from $KEEPER_PASSWORD
+        or parameters as with(user, password) ''' 
+        
+    USER = 'KEEPER_USER'
+    PASSWORD = 'KEEPER_PASSWORD'
+    
+    def __enter__(self, user=None, password=None, user_prompt='User:', password_prompt='Password:'):
+        self.params = params.KeeperParams()
+        if user:
+            self.user = user
+        else: 
+            try:
+                self.params.user = os.environ[KeeperLogin.USER] # getpass.getuser()
+            except:
+                self.params.user = input(user_prompt)
+        if password:
+            self.params.password = password
+        else:
+            try:
+                self.params.password = os.environ[KeeperLogin.PASSWORD]
+            except:
+                self.params.password = getpass.getpass(password_prompt)
+        api.login(self.params)
+        api.sync_down(self.params)
+        return self
 
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+    
 if __name__ == '__main__':
-    try:
-        user = sys.argv[1]
-    except IndexError:
-        try:
-            user = os.environ['user']
-        except KeyError:
-            user = input("User:")
-    try:
-        password = sys.argv[2]
-    except IndexError:
-        try:
-            password = os.environ['password']
-        except KeyError:
-            from getpass import getpass
-            password = getpass('Password:')
-    # config = {'user': user, 'password': password}
-    params = params.KeeperParams()  # config=config)
-    params.user = user
-    params.password = password
-    api.login(params)
-    api.sync_down(params)
-    MAX_REPEAT = 999
+    import logging
+    logger = logging.getLogger(__file__)
     logger.setLevel(logging.INFO)
-    for repeat, uid in enumerate(params.record_cache):
-        if repeat >= MAX_REPEAT:
-            logger.info(f"Exitting because of over limit {repeat}")
-            break
-        print(f"{uid}")
 
-    exit(0) # to suppress warning of 'Exit without exit code'
+    with KeeperLogin() as keeper_login:
+        for uid in keeper_login.params.record_cache:
+            record = api.get_record(keeper_login.params, uid) 
+            print(f"{uid}\t{record.title}")
+
+    exit(0)
