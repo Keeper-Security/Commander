701e7c6170738526a5ef2ae19f17bfdea26940be
Maksim Ustinov <mustinov@keepersecurity.com>
2020-12-29 14:19:47 -0800

    - New Azure AD plugin - Updated algorithm to generate cryptographically secure random password for plugins

---
+++ keepercommander/plugins/azureadpwd/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#  _  __
+# | |/ /___ ___ _ __  ___ _ _ ®
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|
+#
+# Keeper Commander
+# Copyright 2020 Keeper Security Inc.
+# Contact: ops@keepersecurity.com
+#
+
+from .azureadpwd import *
+
+__all__ = ["rotate"]
