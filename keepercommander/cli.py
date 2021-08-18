60a4a3f7c3eae46b666840e7f0d920950c9e9892
Dan Stromberg <dstromberg@keepersecurity.com>
2021-08-18 09:00:20 -0700

    WIP: keepalive 200's, but does not appear to update the activity timestamp in the server?

--- keepercommander/cli.py
+++ keepercommander/cli.py
@@ -24,6 +24,7 @@ from collections import OrderedDict
 from prompt_toolkit import PromptSession
 from prompt_toolkit.shortcuts import CompleteStyle
 from prompt_toolkit.enums import EditingMode
+import pudb
 
 from .commands.msp import get_mc_by_name_or_id
 
@@ -409,6 +410,46 @@ def force_quit():
 prompt_session = None
 
 
+class TimeToKeepalive:
+    """Keep track of how soon the login timer is to expire, and send a keepalive if we're "too close"."""
+    def __init__(self, params):
+        """Initialize."""
+        self.time_of_last_activity = time.time()
+        self.server_logout_timer_window = None
+        self.lookup_server_logout_window(params)
+
+    def lookup_server_logout_window(self, params):
+        """Get the logout_timer value.  IF it doesn't exist yet, use None."""
+        if self.server_logout_timer_window is not None:
+            # We already have it.
+            return
+        if params.settings is not None and 'logout_timer' in params.settings:
+            # We should be able to get it.
+            print('fred 2: getting self.server_logout_timer_window')
+            self.server_logout_timer_window = float(params.settings['logout_timer']) / 1000.0
+
+    def update(self, params):
+        """Update the timer, and possibly issue a keepalive."""
+        print('fred 1: update-ing')
+        current_time = time.time()
+
+        self.lookup_server_logout_window(params)
+
+        if (
+                self.server_logout_timer_window is not None and
+                (self.server_logout_timer_window / 2) + self.time_of_last_activity < current_time
+        ):
+            print('fred 3: sending a keepalive')
+            send_keepalive(params)
+            self.time_of_last_activity = current_time
+
+
+def send_keepalive(params):
+    """Send a keepalive to the server, using protobufs."""
+    pudb.set_trace()  # singlestepping in pudb shows this 200'ing the first time.
+    api.communicate_rest(params, None, 'keep_alive')
+
+
 def loop(params):  # type: (KeeperParams) -> int
     logging.debug('Params: %s', params)
 
@@ -451,7 +492,11 @@ def loop(params):  # type: (KeeperParams) -> int
         except Exception as e:
             logging.error(e)
 
+    TTK = TimeToKeepalive(params)
+
     while True:
+        TTK.update(params)
+
         command = ''
         if len(params.commands) > 0:
             command = params.commands[0].strip()
