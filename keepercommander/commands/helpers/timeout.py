82820a6aa0bce762b7ea9362c09f9bcb9512ffef
Kevin Kihneman <kkihneman@keepersecurity.com>
2021-08-31 13:37:40 -0700

    Add docstrings and minor update to timeout helper functions

--- keepercommander/commands/helpers/timeout.py
+++ keepercommander/commands/helpers/timeout.py
@@ -8,6 +8,14 @@ from keepercommander.constants import (
 
 
 def parse_timeout(timeout_input):
+    """Parse timeout input to return instance of timedelta
+
+    timeout_input(str): String to parse for one or more integer values followed by time unit names. The allowed time
+        units are found in the list constants.TIMEOUT_ALLOWED_UNITS. Any substring from the beginning of the allowed
+        unit may be provided. If no time unit names are included, then the default time unit from
+        constants.TIMEOUT_DEFAULT_UNIT is used. An error is raised if a unit name in timeout_input is unrecognized.
+    Returns instance of timedelta from the unit values and names.
+    """
     if timeout_input.strip().isnumeric():
         tdelta_kwargs = {TIMEOUT_DEFAULT_UNIT: int(timeout_input)}
     else:
@@ -22,6 +30,12 @@ def parse_timeout(timeout_input):
 
 
 def format_timeout(timeout_delta):
+    """Format timeout as instance of timedelta for output to console
+
+    timeout_delta(timedelta): Timeout setting as instance of timedelta.
+    Returns string with value and names of time units separated by commas. If all unit values are zero,
+        then the string '0' is returned.
+    """
     if timeout_delta == timedelta(0):
         return '0'
     else:
@@ -35,25 +49,41 @@ def format_timeout(timeout_delta):
 
 
 def enforce_timeout_range(timeout_delta):
+    """Enforce the range of allowed timeout values based on constants TIMEOUT_MIN and TIMEOUT_MAX
+    Warnings are raised if the timeout is outside of the range.
+
+    timeout_delta(timedelta): Timeout setting as instance of timedelta.
+    Returns instance of timedelta. If the timeout value is greater than TIMEOUT_MAX, then TIMEOUT_MAX is returned.
+        If the timeout value is less than TIMEOUT_MIN, then TIMEOUT_DEFAULT is returned.
+    """
     if timeout_delta <= TIMEOUT_MIN:
-        timeout_delta = TIMEOUT_DEFAULT
         logging.warning(
             f'The minimum device timeout value is {format_timeout(TIMEOUT_MIN)}. '
             'The device timeout has been set to the default Keeper timeout value.'
         )
+        return TIMEOUT_DEFAULT
     elif timeout_delta > TIMEOUT_MAX:
-        timeout_delta = TIMEOUT_MAX
         logging.warning(
             f'The maximum device timeout value is {format_timeout(TIMEOUT_MAX)}. '
             'The device timeout has been set to the maximum.'
         )
-    return timeout_delta
+        return TIMEOUT_MAX
 
 
 def get_timeout_setting_from_delta(timeout_delta):
+    """Get timeout setting in minutes to be used in an API call to the Keeper backend
+
+    timeout_delta(timedelta): Timeout setting as instance of timedelta.
+    Returns string representation of the integer value of timeout minutes.
+    """
     timeout_seconds = int(timeout_delta.total_seconds() // 60)
     return str(timeout_seconds)
 
 
 def get_delta_from_timeout_setting(timeout_setting):
+    """Get the timeout as an instance of timedelta from the setting returned from the API call
+
+    timeout_setting(str): Timeout setting as a string representation of the integer value of timeout milliseconds.
+    Returns timedelta instance.
+    """
     return timedelta(milliseconds=int(timeout_setting))
