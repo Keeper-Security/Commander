35da943594170e4385e2f26b350ad45380a3a018
Maksim Ustinov <mustinov@keepersecurity.com>
2021-08-31 07:57:13 -0700

    Manually downgrading client version to 16.1.6
    Bumping Commander version to 16.1.8

--- keepercommander/rest_api.py
+++ keepercommander/rest_api.py
@@ -27,7 +27,7 @@ from . import __version__
 
 
 LEGACY_CLIENT_VERSION = 'c14.0.0'
-CLIENT_VERSION = 'c' + __version__
+CLIENT_VERSION = 'c16.1.6'
 
 SERVER_PUBLIC_KEYS = {
     1: RSA.importKey(base64.urlsafe_b64decode(
