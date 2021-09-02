90f20b0a81cca7ea9e87b04816cd18037ce205d3
Kevin Kihneman <kihneman@users.noreply.github.com>
2021-09-02 14:55:47 -0700

    Add ksm command that passes arguments to ksm cli (KC-252) (#465)
    * Add ksm command to commands.utils
    
    * Add '--' before ksm command arguments
    
    * Check for ksm application before running commands
    
    * Pass args to ksm when detected in keepercommander.cli
    
    * Remove ksm from keepercommander.commands

--- keepercommander/cli.py
+++ keepercommander/cli.py
@@ -18,6 +18,7 @@ import threading
 import functools
 import logging
 import re
+import subprocess
 
 from collections import OrderedDict
 
@@ -175,7 +176,16 @@ def do_command(params, command_line):
         display.formatted_history(stack)
         return
 
-    if '-h' in command_line.lower():
+    if command_line.startswith('ksm'):
+        try:
+            which_cmd = 'where' if sys.platform.startswith('win') else 'which'
+            subprocess.check_call([which_cmd, 'ksm'])
+        except subprocess.CalledProcessError:
+            logging.error('Please install the ksm application to run ksm commands.')
+        else:
+            subprocess.check_call(command_line)
+        return
+    elif '-h' in command_line.lower():
         if command_line.lower().startswith('h ') or command_line.lower().startswith('history '):
             print("usage: history|h [-h]")
             print("\nShow command history.")
