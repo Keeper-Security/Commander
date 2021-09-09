6e95af536b500ec6ff7a6342278d225f983f7ca9
Dan Stromberg <dstromberg@keepersecurity.com>
2021-09-09 08:09:26 -0700

    Removed
    Ignore Makefile
    
    Parameterized many tests

--- unit-tests/test_autocomplete.py
+++ unit-tests/test_autocomplete.py
@@ -2,9 +2,9 @@
 
 """Tests for keeper interactive shell autocompletion."""
 
-from unittest import TestCase
+import pytest
 
-# This crufty import makes autocomplete import successfully.
+# This crufty import makes keepercommander.autocomplete import successfully.
 import keepercommander.cli
 
 import keepercommander.autocomplete as autocomplete
@@ -27,170 +27,36 @@ def check_roundtrip_wo_in_double_quotes(string):
     assert string == unescaped
 
 
-class TestAutocompleteQuotingWithInDoubleQuotes(TestCase):
-    """Tests for keeper interactive shell autocompletion - quoting."""
-
-    def test_backslash_quote_k(self):
-        r"""Test if '\"k' works with in_double_quotes."""
-        check_roundtrip_w_in_double_quotes('\\"k')
-
-    def test_a_slash_slash(self):
-        """Test if 'a//' works."""
-        check_roundtrip_w_in_double_quotes('a//')
-
-    def test_c_space(self):
-        r"""Test if 'c\ ' works."""
-        check_roundtrip_w_in_double_quotes('c\\ ')
-
-    def test_e_blackslash_apostrophe(self):
-        r"""Test if 'e\'f' works."""
-        check_roundtrip_w_in_double_quotes(r"e\'f")
-
-    def test_g_backslash_backslash_double_quote(self):
-        r"""Test if 'g\"' works."""
-        check_roundtrip_w_in_double_quotes(r'g\"')
-
-    def test_i_backslash(self):
-        r"""Test if 'i\' works."""
-        check_roundtrip_w_in_double_quotes('i\\')
-
-    def test_k_backslash_double_quote(self):
-        r"""Test if 'k\"' works."""
-        check_roundtrip_w_in_double_quotes('k\\"')
-
-    def test_r_backslash_s(self):
-        r"""Test if 'r\s' works."""
-        check_roundtrip_w_in_double_quotes(r'r\s')
-
-    def test_double_quote_backslash_k(self):
-        r"""Test if '"\"k' works."""
-        check_roundtrip_w_in_double_quotes(r'"\"k')
-
-    def test_double_quote_a_slash_slash(self):
-        """Test if '"a//' works."""
-        check_roundtrip_w_in_double_quotes('"a//')
-
-    def test_double_quote_c_space(self):
-        """Test if '"c ' works."""
-        check_roundtrip_w_in_double_quotes('"c ')
-
-    def test_double_quote_e_apostrophe(self):
-        """Test if '"e'f' works."""
-        check_roundtrip_w_in_double_quotes('"e\'f')
-
-    def test_double_quote_g_backslash_double_quote(self):
-        r"""Test if '"g\"' works."""
-        check_roundtrip_w_in_double_quotes(r'"g\"')
-
-    def test_double_quote_i_backslash_backslash(self):
-        r"""Test if '"i\\' works."""
-        check_roundtrip_w_in_double_quotes(r'"i\\')
-
-    def test_double_quote_k_backslash_double_quote(self):
-        r"""Test if '"k\"' works."""
-        check_roundtrip_w_in_double_quotes(r'"k\"')
-
-    def test_double_quote_r_backslash(self):
-        r"""Test if '"r\\' works."""
-        check_roundtrip_w_in_double_quotes(r'"r\\')
-
-    def test_apostrophe(self):
-        r"""Test if "'" works."""
-        check_roundtrip_w_in_double_quotes(r"'")
-
-    def test_apostrophe_apostrophe(self):
-        r"""Test if "''" works."""
-        check_roundtrip_w_in_double_quotes(r"''")
-
-    def test_apostrophe_a_apostrophe(self):
-        r"""Test if "'a'" works."""
-        check_roundtrip_w_in_double_quotes(r"'a'")
+strings_to_test = (
+    '\\"k',
+    'a//',
+    'c\\ ',
+    r"e\'f",
+    r'g\"',
+    'i\\',
+    'k\\"',
+    r'r\s',
+    r'"\"k',
+    '"a//',
+    '"c ',
+    '"e\'f',
+    r'"g\"',
+    r'"i\\',
+    r'"k\"',
+    r'"r\\',
+    r"'",
+    r"''",
+    r"'a'",
+)
 
 
-class TestAutocompleteQuotingWithoutInDoubleQuotes(TestCase):
-    """Tests for keeper interactive shell autocompletion - quoting."""
-
-    def test_backslash_quote_k(self):
-        r"""Test if '\"k' works with in_double_quotes."""
-        check_roundtrip_wo_in_double_quotes('\\"k')
-
-    def test_a_slash_slash(self):
-        """Test if 'a//' works."""
-        check_roundtrip_wo_in_double_quotes('a//')
-
-    def test_c_space(self):
-        r"""Test if 'c\ ' works."""
-        check_roundtrip_wo_in_double_quotes('c\\ ')
-
-    def test_e_blackslash_apostrophe(self):
-        r"""Test if 'e\'f' works."""
-        check_roundtrip_wo_in_double_quotes(r"e\'f")
-
-    def test_g_backslash_backslash_double_quote(self):
-        r"""Test if 'g\"' works."""
-        check_roundtrip_wo_in_double_quotes(r'g\"')
-
-    def test_i_backslash(self):
-        r"""Test if 'i\' works."""
-        check_roundtrip_wo_in_double_quotes('i\\')
-
-    def test_k_backslash_double_quote(self):
-        r"""Test if 'k\"' works."""
-        check_roundtrip_wo_in_double_quotes('k\\"')
-
-    def test_r_backslash_s(self):
-        r"""Test if 'r\s' works."""
-        check_roundtrip_wo_in_double_quotes(r'r\s')
-
-    def test_double_quote_backslash_k(self):
-        r"""Test if '"\"k' works."""
-        check_roundtrip_wo_in_double_quotes(r'"\"k')
-
-    def test_double_quote_a_slash_slash(self):
-        """Test if '"a//' works."""
-        check_roundtrip_wo_in_double_quotes('"a//')
-
-    def test_double_quote_c_space(self):
-        """Test if '"c ' works."""
-        check_roundtrip_wo_in_double_quotes('"c ')
-
-    def test_double_quote_e_apostrophe(self):
-        """Test if '"e'f' works."""
-        check_roundtrip_wo_in_double_quotes('"e\'f')
-
-    def test_double_quote_g_backslash_double_quote(self):
-        r"""Test if '"g\"' works."""
-        check_roundtrip_wo_in_double_quotes(r'"g\"')
-
-    def test_double_quote_i_backslash_backslash(self):
-        r"""Test if '"i\\' works."""
-        check_roundtrip_wo_in_double_quotes(r'"i\\')
-
-    def test_double_quote_k_backslash_double_quote(self):
-        r"""Test if '"k\"' works."""
-        check_roundtrip_wo_in_double_quotes(r'"k\"')
-
-    def test_double_quote_r_backslash(self):
-        r"""Test if '"r\\' works."""
-        check_roundtrip_wo_in_double_quotes(r'"r\\')
-
-    def test_apostrophe(self):
-        r"""Test if "\"'" works."""
-        check_roundtrip_wo_in_double_quotes(r"\"'")
-
-    def test_apostrophe_apostrophe(self):
-        r"""Test if "\"''" works."""
-        check_roundtrip_wo_in_double_quotes(r"\"''")
-
-    def test_apostrophe_a_apostrophe(self):
-        r"""Test if "\"'a'" works."""
-        check_roundtrip_wo_in_double_quotes(r"\"'a'")
+@pytest.mark.parametrize('string_to_test', strings_to_test)
+def test_w_double_quotes(string_to_test):
+    r"""Test if a string round trips with in_double_quotes."""
+    check_roundtrip_w_in_double_quotes(string_to_test)
 
 
-if __name__ == '__main__':
-    instance = TestAutocompleteQuotingWithInDoubleQuotes()
-    if hasattr(instance, 'setUp'):
-        instance.setUp()
-    instance.test_backslash_quote_k()
-    if hasattr(instance, 'tearDown'):
-        instance.tearDown()
+@pytest.mark.parametrize('string_to_test', strings_to_test)
+def test_wo_double_quotes(string_to_test):
+    r"""Test if a string round trips without in_double_quotes."""
+    check_roundtrip_wo_in_double_quotes(string_to_test)
