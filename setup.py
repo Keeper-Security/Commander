fd477e976d97214c866bd21b1324b114b04d7bf6
Kevin Kihneman <kkihneman@keepersecurity.com>
2021-09-13 22:59:10 -0500

    Fix setup.py setup formatting

--- setup.py
+++ setup.py
@@ -24,35 +24,36 @@ adpasswd_requires = ['ldap3']
 keepass_requires = ['libkeepass']
 test_requires = ['pytest', 'testfixtures']
 
-setup(name='keepercommander',
-      version=keepercommander.__version__,
-      description='Keeper Commander for Python 3',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      author='Craig Lurey',
-      author_email='craig@keepersecurity.com',
-      url='https://github.com/Keeper-Security/Commander',
-      license='MIT',
-      classifiers=["Development Status :: 4 - Beta",
-                   "License :: OSI Approved :: MIT License",
-                   "Operating System :: OS Independent",
-                   "Programming Language :: Python :: 3.6",
-                   "Topic :: Security"],
-      keywords='security password',
+setup(
+    name='keepercommander',
+    version=keepercommander.__version__,
+    description='Keeper Commander for Python 3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Craig Lurey',
+    author_email='craig@keepersecurity.com',
+    url='https://github.com/Keeper-Security/Commander',
+    license='MIT',
+    classifiers=["Development Status :: 4 - Beta",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3.6",
+                 "Topic :: Security"],
+    keywords='security password',
 
-      packages=find_packages(),
-      include_package_data=True,
-      python_requires='>=3.6',
-      entry_points={
-          "console_scripts": [
-              "keeper=keepercommander.__main__:main",
-          ],
-      },
-      install_requires=install_requires,
-      extras_require={
-          'adpasswd': adpasswd_requires,
-          'keepass': keepass_requires,
-          'test': test_requires,
-          'all': adpasswd_requires + keepass_requires + test_requires
-      }
-      )
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "keeper=keepercommander.__main__:main",
+        ],
+    },
+    install_requires=install_requires,
+    extras_require={
+        'adpasswd': adpasswd_requires,
+        'keepass': keepass_requires,
+        'test': test_requires,
+        'all': adpasswd_requires + keepass_requires + test_requires
+    }
+)
