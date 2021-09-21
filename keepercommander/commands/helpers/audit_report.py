abee3041bac7ccb0c74a3ab10a7b1eda9d9dd144
Kevin Kihneman <kkihneman@keepersecurity.com>
2021-09-21 13:45:34 -0500

    Add audit-report helpers

---
+++ keepercommander/commands/helpers/audit_report.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+
+
+MISC_FIELDS = (
+    'to_username', 'from_username', 'record_uid', 'shared_folder_uid', 'node', 'channel', 'status'
+)
+
+
+@dataclass
+class LookupType:
+    """Helper class for looking up fields given the uid"""
+    uid: str
+    fields: tuple
+    attrs: tuple
+    method: str
+
+    @staticmethod
+    def lookup_type_from_field_name(field_name: str):
+        uid_name = fields_to_uid_name[field_name]
+        return lookup_types[uid_name]
+
+    def init_fields(self, init_value: str) -> dict:
+        return dict.fromkeys(self.fields, init_value)
+
+    def field_attrs(self) -> zip:
+        return zip(self.fields, self.attrs)
+
+
+lookup_types = {
+    'record_uid': LookupType(
+        uid='record_uid',
+        fields=('record_title', 'record_url'),
+        attrs=('title', 'login_url'),
+        method='resolve_record_lookup'
+    ),
+    'team_uid': LookupType(
+        uid='team_uid', fields=('team_title',), attrs=('name',), method='resolve_team_lookup'
+    ),
+    'role_id': LookupType(
+        uid='role_id', fields=('role_title',), attrs=('displayname',), method='resolve_role_lookup'
+    ),
+    'node_id': LookupType(
+        uid='node_id', fields=('node_title',), attrs=('displayname',), method='resolve_node_lookup'
+    ),
+}
+
+fields_to_uid_name = {}
+
+for k, v in lookup_types.items():
+    for f in v.fields:
+        fields_to_uid_name[f] = k
