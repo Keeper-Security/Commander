16c72124e2b8e188bb6e416df9ee9b91116abe23
Sergey Kolupaev <skolupaev@keepersecurity.com>
2021-09-30 11:44:09 -0700

    LastPass import: do not resolve users if total number of shared folder exceeds 50

--- keepercommander/importer/lastpass/shared_folder.py
+++ keepercommander/importer/lastpass/shared_folder.py
@@ -1,5 +1,5 @@
 class LastpassSharedFolder:
-    def __init__(self, id, name, members, teams):
+    def __init__(self, id, name, members=None, teams=None):
         self.id = id
         self.name = name
         self.members = members
--- keepercommander/importer/lastpass/vault.py
+++ keepercommander/importer/lastpass/vault.py
@@ -12,14 +12,15 @@ class Vault(object):
         session = fetcher.login(username, password, multifactor_password, client_id)
         blob = fetcher.fetch(session)
         encryption_key = blob.encryption_key(username, password)
-        return cls(blob, encryption_key, session)
+        vault = cls(blob, encryption_key, session)
+        fetcher.logout(session)
+        return vault
 
     @classmethod
     def open_local(cls, blob_filename, username, password):
         """Creates a vault from a locally stored blob"""
         # TODO: read the blob here
         raise NotImplementedError()
-
 
     def __init__(self, blob, encryption_key, session):
         """This more of an internal method, use one of the static constructors instead"""
@@ -30,12 +31,25 @@ class Vault(object):
 
         self.errors = set()
         self.shared_folders = []
-        self.accounts = self.parse_accounts(chunks, encryption_key, session)
+        self.accounts = self.parse_accounts(chunks, encryption_key)
+
+        try:
+            if self.shared_folders and len(self.shared_folders) < 50:
+                for shared_folder in self.shared_folders:
+                    members, teams, error = fetcher.fetch_shared_folder_members(session, shared_folder.id)
+                    if error:
+                        self.errors.add(error)
+                        break
+                    else:
+                        shared_folder.members = members
+                        shared_folder.teams = teams
+        except:
+            pass
 
     def is_complete(self, chunks):
         return len(chunks) > 0 and chunks[-1].id == b'ENDM' and chunks[-1].payload == b'OK'
 
-    def parse_accounts(self, chunks, encryption_key, session):
+    def parse_accounts(self, chunks, encryption_key):
         accounts = []
 
         key = encryption_key
@@ -54,11 +68,7 @@ class Vault(object):
                 share = parser.parse_SHAR(i, encryption_key, rsa_private_key)
                 key = share['encryption_key']
                 shareid = share['id'].decode('utf-8')
-                members, teams, error = fetcher.fetch_shared_folder_members(session, shareid)
-                if error:
-                    self.errors.add(error)
-                shared_folder = LastpassSharedFolder(shareid, share['name'].decode('utf-8'), members, teams)
+                shared_folder = LastpassSharedFolder(shareid, share['name'].decode('utf-8'))
                 self.shared_folders.append(shared_folder)
 
-        fetcher.logout(session)
         return accounts
