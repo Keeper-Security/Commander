6eab4ef6f440cca8b1f195757620725c3cfc3722
Dan Stromberg <dstromberg@keepersecurity.com>
2021-10-01 09:37:12 -0700

    Updated for V3. pylint applied. Updated comments
    Do not traceback when params.config_filename is None

--- keepercommander/custom/add_record_to_shared_folder.py
+++ keepercommander/custom/add_record_to_shared_folder.py
@@ -5,34 +5,34 @@
 #              |_|
 #
 # Keeper Commander
-# Copyright 2018 Keeper Security Inc.
+# Copyright 2021 Keeper Security Inc.
 # Contact: commander@keepersecurity.com
-#
-# Example showing how to add a new or existing record 
-# to an existing shared folder. 
-#
+
+"""Demonstrate how to add a new or existing single record to an existing shared folder."""
+
 import os
 
 from keepercommander import api
 from keepercommander.__main__ import get_params_from_config
-from keepercommander.commands.record import RecordAddCommand
+from keepercommander.commands.recordv3 import RecordAddCommand
 from keepercommander.params import KeeperParams
 
 
 def login(
-        email,
-        password=None,
-        keeper_server='https://keepersecurity.com/api/v2/',
-        config_file='myconfig.json'):
-
-    if os.path.exists(config_file):
+    password=None,
+    config_file='myconfig.json',
+):
+    """Get parameters from either the config file or function arguments, and log in."""
+    if config_file and os.path.exists(config_file):
         params = get_params_from_config(config_file)
     else:
         params = KeeperParams()
+        # If we set config_filename to a filename, and it doesn't pre-exist, it will be created with minimal content that perplexes
+        # subsequent runs.
         params.config_filename = config_file
-        params.user = email
+        params.user = 'your-keeper-account-email@keepersecurity.com'
         params.password = password if password else ''
-        params.server = keeper_server
+        params.server = 'https://keepersecurity.com/api/rest'
         params.config['server'] = params.server
 
     api.login(params)
@@ -42,7 +42,9 @@ def login(
 
 
 def create_new_record(params):
-    shared_folder_uid = 'UID'
+    """Create a record."""
+    # Fill in a shared folder UID here.  You can obtain a UID for a shared folder from ls -l in Keeper Commander.
+    shared_folder_uid = 'sfusfusfusfusfusfusfsf'
 
     command = RecordAddCommand()
 
@@ -55,14 +57,27 @@ def create_new_record(params):
         folder=shared_folder_uid,
         generate=True,
         # force=True disables asking for missing fields
-        force=True)
+        force=True,
+    )
 
     print('Added record %s to shared folder uid=%s' % (record_uid, shared_folder_uid))
 
 
-if __name__ == '__main__':
+def main():
+    """Login and create the record."""
+    # If you want to use function arguments instead of a config file, use this line.
+    params = login(config_file=None)
 
-    # Login and sync
-    params = login("user@email.com", password="password")
+    # Or if you want to use a myconfig.json, use this 'params = login()' line.  Your myconfig.json should look something like:
+    # {
+    #     "server": "keepersecurity.com",
+    #     "user": "your-keeper-account-email@keepersecurity.com"
+    # }
+    # Note that Commander may add more fields to this file.
+    # params = login()
 
     create_new_record(params)
+
+
+if __name__ == '__main__':
+    main()
