eb7fa492f2a3ebf9682d7dda0728c8a4e9de8ee6
Kevin Kihneman <kkihneman@keepersecurity.com>
2021-10-04 09:33:03 -0500

    Fix logging level with 'setLevel'

--- keepercommander/__main__.py
+++ keepercommander/__main__.py
@@ -209,7 +209,8 @@ def main(from_package=False):
         if opts.command == '?' or not params.commands:
             usage('')
 
-    logging.basicConfig(level=logging.WARNING if params.batch_mode else logging.INFO, format='%(message)s')
+    logging.basicConfig(format='%(message)s')
+    logging.getLogger().setLevel(logging.WARNING if params.batch_mode else logging.INFO)
 
     if params.timedelay >= 1 and params.commands:
         cli.runcommands(params)
