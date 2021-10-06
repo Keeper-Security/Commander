83b609ac75c343c064d83730a5a2956219503da8
Dan Stromberg <dstromberg@keepersecurity.com>
2021-10-06 14:49:11 -0700

    Brought back from the dead. Support V3 records. Linted

--- keepercommander/custom/add_record_to_shared_folder.py
+++ keepercommander/custom/add_record_to_shared_folder.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 #  _  __
 # | |/ /___ ___ _ __  ___ _ _ ®
 # | ' </ -_) -_) '_ \/ -_) '_|
@@ -8,81 +7,77 @@
 # Keeper Commander
 # Copyright 2021 Keeper Security Inc.
 # Contact: commander@keepersecurity.com
-#
-# Example showing how to add a new or existing record 
-# to an existing shared folder. 
-#
 
-import getopt
+"""Demonstrate how to add a new or existing single record to an existing shared folder."""
+
 import os
-import sys
 
 from keepercommander import api
 from keepercommander.__main__ import get_params_from_config
-from keepercommander.commands.record import RecordAddCommand, RecordEditCommand
+from keepercommander.commands.recordv3 import RecordAddCommand
 from keepercommander.params import KeeperParams
 
 
-def login_to_keeper_with_config(filename):   # type: (str) -> KeeperParams
-    # Load connection parameters from file
-    my_params = get_params_from_config(filename)
-    my_params.password = ''  # Keeper account password. Can be omitted if account is setup for session resumption
-    # Login to Keeper
-    api.login(my_params)
-    api.sync_down(my_params)
-    return my_params
-
-
-def create_or_update_record(params, criteria):
-    # Scan vault for shared folder
-
-    sfs = api.search_shared_folders(params, criteria)
-    if len(sfs) == 0:
-        print(f'Shared folder {criteria} not found')
-        return
-    shared_folder = sfs[0]
-    record_title = 'Test Record'
-    record_uid = None
-    if shared_folder.records:
-        records = [api.get_record(params, x['record_uid']) for x in shared_folder.records]
-        record_uid = next((x.record_uid for x in records if x.title.casefold() == record_title.casefold()), None)
-
-    shared_folder_uid = shared_folder.shared_folder_uid
-    if record_uid:
-        command = RecordEditCommand()
-        command.execute(params, record=record_uid, generate=True)
-        print(f'Updated record {record_uid} in shared folder uid={shared_folder_uid}')
+def login(
+    password=None,
+    config_file='myconfig.json',
+):
+    """Get parameters from either the config file or function arguments, and log in."""
+    if config_file and os.path.exists(config_file):
+        params = get_params_from_config(config_file)
     else:
-        command = RecordAddCommand()
-        # Inputs - hard coded for demo purposes
-        record_uid = command.execute(
-            params,
-            title='Test Record',
-            login='someone@company.com',
-            folder=shared_folder_uid,
-            generate=True,
-            # force=True disables asking for missing fields
-            force=True)
-        print(f'Added record {record_uid} to shared folder uid={shared_folder_uid}')
+        params = KeeperParams()
+        # If we set config_filename to a filename, and it doesn't pre-exist, it will be created with minimal content that perplexes
+        # subsequent runs.
+        params.config_filename = config_file
+        params.user = 'your-keeper-account-email@keepersecurity.com'
+        params.password = password if password else ''
+        params.server = 'https://keepersecurity.com/api/rest'
+        params.config['server'] = params.server
+
+    api.login(params)
+    api.sync_down(params)
+
+    return params
+
+
+def create_new_record(params):
+    """Create a record."""
+    # Fill in a shared folder UID here.  You can obtain a UID for a shared folder from ls -l in Keeper Commander.
+    shared_folder_uid = 'sfusfusfusfusfusfusfsf'
+
+    command = RecordAddCommand()
+
+    # Inputs - hard coded for demo purposes
+    record_uid = command.execute(
+        params,
+        title='Test Record',
+        login='someone@company.com',
+        url='https://google.com',
+        folder=shared_folder_uid,
+        generate=True,
+        # force=True disables asking for missing fields
+        force=True,
+    )
+
+    print('Added record %s to shared folder uid=%s' % (record_uid, shared_folder_uid))
+
+
+def main():
+    """Login and create the record."""
+    # If you want to use function arguments instead of a config file, use this line.
+    params = login(config_file=None)
+
+    # Or if you want to use a myconfig.json, use this 'params = login()' line.  Your myconfig.json should look something like:
+    # {
+    #     "server": "keepersecurity.com",
+    #     "user": "your-keeper-account-email@keepersecurity.com"
+    # }
+    # Note that Commander may add more fields to this file.
+    # params = login()
+
+    create_new_record(params)
 
 
 if __name__ == '__main__':
-    opts, args = getopt.getopt(sys.argv[1:], 'c:', ['config='])
-    if len(args) == 0:
-        print('Shared folder name or UID parameter is required.')
-        sys.exit(1)
-
-    config_file = 'myconfig.json'
-    for p_name, p_value in opts:
-        if p_name == '-c' or p_name == '--config':
-            config_file = p_value
-
-    if not os.path.exists(config_file):
-        print(f'Config file {config_file} not found')
-        sys.exit(1)
-
-    # Login and sync
-    params = login_to_keeper_with_config(config_file)
-
-    # Add record to shared folder
-    create_or_update_record(params, args[0])
+    main()
