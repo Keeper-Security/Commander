34d99645f91bd1c61cd70b24361cd9c6b283783f
Dan Stromberg <dstromberg@keepersecurity.com>
2021-10-06 14:08:40 -0700

    Linting, allow cmdr:port for nondefault ssh port

--- keepercommander/plugins/ssh/ssh.py
+++ keepercommander/plugins/ssh/ssh.py
@@ -1,45 +1,65 @@
 # -*- coding: utf-8 -*-
-#  _  __  
+#  _  __
 # | |/ /___ ___ _ __  ___ _ _ ®
 # | ' </ -_) -_) '_ \/ -_) '_|
 # |_|\_\___\___| .__/\___|_|
-#              |_|            
+#              |_|
 #
-# Keeper Commander 
+# Keeper Commander
 # Copyright 2016 Keeper Security Inc.
 # Contact: ops@keepersecurity.com
 #
 
+"""Change a *ix password over ssh."""
+
+
 import logging
 import os
-from ...error import Error
 
 if os.name == 'posix':
     from pexpect import pxssh, exceptions
 else:
     raise Exception('Not available on Windows')
 
-"""Commander Plugin for SSH Command
-   Dependencies: s
-       pip3 install pexpect
-"""
 
 def rotate(record, newpassword):
-    """ Grab any required fields from the record """
+    """
+    Change a *ix password over ssh.
 
+    We're pretty platform-agnostic - just need a passwd command.  But pxssh is not.  pxssh wants a sh/ksh/bash/csh/tcsh.  zsh
+    (macOS' 11.* default shell) doesn't work.  And routers are unlikely to work too, even if they support ssh, especially
+    ones with dynamic prompts.
+
+    Grab any required fields from the record.
+    """
     user = record.login
     oldpassword = record.password
 
     result = False
 
+    optional_port = record.get('cmdr:port')
+    if not optional_port:
+        port = 22
+    else:
+        try:
+            port = int(optional_port)
+        except ValueError:
+            print('port {} could not be converted to int'.format(optional_port))
+            return result
+
     host = record.get('cmdr:host')
 
     try:
-        s = pxssh.pxssh()
-        s.login(host, user, oldpassword, sync_multiplier=3)
+        options = {
+            'StrictHostKeyChecking': 'no',
+            'UserKnownHostsFile': '/dev/null',
+        }
+        s = pxssh.pxssh(options=options)
+        print('Logging into {}@{} on port {}'.format(user, host, port))
+        s.login(server=host, username=user, password=oldpassword, sync_multiplier=3, port=port)
         s.sendline('passwd')
         i = s.expect(['[Oo]ld.*[Pp]assword', '[Cc]urrent.*[Pp]assword', '[Nn]ew.*[Pp]assword'])
-        if i == 0 or i == 1:
+        if i in (0, 1):
             s.sendline(oldpassword)
             i = s.expect(['[Nn]ew.*[Pp]assword', 'password unchanged'])
             if i != 0:
@@ -50,20 +70,26 @@ def rotate(record, newpassword):
         s.sendline(newpassword)
         s.prompt()
 
-        pass_result = s.before
+        pass_result = str(s.before).lower()
 
-        if "success" in str(pass_result):
+        if "success" in pass_result:
             logging.info("Password changed successfully")
             record.password = newpassword
             result = True
+        elif 'this tool does not update the login keychain password' in pass_result.lower():
+            # This is a macOS thing.  The passwd command warns about updating the keychain.  We pass a little of that
+            # up to the user.
+            logging.info("Password changed successfully")
+            logging.info("Consider updating login keychain with: security set-keychain-password ~/Library/Keychains/login-db")
+            record.password = newpassword
+            result = True
         else:
-            logging.error("Password change failed: ", pass_result)
+            logging.error("Password change failed: {}".format(str(pass_result)))
 
         s.logout()
-    except exceptions.TIMEOUT as t:
+    except exceptions.TIMEOUT:
         logging.error("Timed out waiting for response.")
     except pxssh.ExceptionPxssh as e:
-        logging.error("Failed to login with ssh.")
-        logging.error(e)
+        logging.error("Failed to login with ssh: {}".format(str(e)))
 
     return result
