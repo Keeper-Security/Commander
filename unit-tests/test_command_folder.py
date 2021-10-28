229ceaf3b7a678c3aca31b1202a7888ed7c6983f
Kevin Kihneman <kkihneman@keepersecurity.com>
2021-10-27 23:11:49 -0500

    Fix tests for rmdir with execute api

--- unit-tests/test_command_folder.py
+++ unit-tests/test_command_folder.py
@@ -120,7 +120,7 @@ class TestFolder(TestCase):
             self.assertTrue(KeeperApiHelper.is_expect_empty())
 
             mock_choice.return_value = 'y'
-            KeeperApiHelper.communicate_expect([pre_delete, 'delete'])
+            KeeperApiHelper.communicate_expect([pre_delete, 'execute'])
             cmd.execute(params, pattern=[user_folder.name])
             self.assertTrue(KeeperApiHelper.is_expect_empty())
 
@@ -130,9 +130,13 @@ class TestFolder(TestCase):
         shared_folder = next(iter([x for x in params.folder_cache.values() if x.type == 'shared_folder']))
 
         def shared_folder_update(rq):
-            self.assertEqual(rq['command'], 'shared_folder_update')
-            self.assertEqual(rq['operation'], 'delete')
-            self.assertEqual(rq['shared_folder_uid'], shared_folder.uid)
+            requests = [{
+                'command': 'shared_folder_update',
+                'operation': 'delete',
+                'shared_folder_uid': shared_folder.uid
+            }]
+            self.assertEqual(rq['command'], 'execute')
+            self.assertEqual(rq['requests'], requests)
 
         KeeperApiHelper.communicate_expect([shared_folder_update])
         cmd.execute(params, force=True, pattern=[shared_folder.name])
