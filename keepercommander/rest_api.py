61d628ef434d3bfe3e86882b3e992799e3352636
Sergey Kolupaev <skolupaev@keepersecurity.com>
2022-06-17 14:08:16 -0700

    Release 16.6.9

--- keepercommander/rest_api.py
+++ keepercommander/rest_api.py
@@ -27,7 +27,7 @@ from . import __version__
 
 LEGACY_CLIENT_VERSION = 'c14.0.0'
 # CLIENT_VERSION = 'c' + __version__
-CLIENT_VERSION = 'c16.6.2'
+CLIENT_VERSION = 'c16.6.9'
 
 SERVER_PUBLIC_KEYS = {
     1: crypto.load_rsa_public_key(utils.base64_url_decode(
