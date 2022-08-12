9aed425567513f293594ccf6a28788f8ddac58a3
Ayrris Aunario <aaunario@keepersecurity.com>
2022-08-12 16:34:30 -0500

    re-factored SoxData for better readability, fixed type comments to remove IDE warnings, fixed user data re-loading when not in full-sync

--- keepercommander/sox/sox_data.py
+++ keepercommander/sox/sox_data.py
@@ -1,72 +1,69 @@
-from typing import Iterable, Dict, Set, List
-from . import sox_types
+from typing import Iterable, Dict, Set, List, Optional
+from . import sox_types, sqlite_storage
+
 
 class RebuildTask:
     def __init__(self, is_full_sync):      # type: (bool) -> None
         self.is_full_sync = is_full_sync   # type: bool
         self.records = set()               # type: Set[str]
 
-    def update_records(self, record_uids):    # type: (Iterable[str]) -> None
+    def update_records(self, record_ids):    # type: (Iterable[str]) -> None
         if self.is_full_sync:
             return
-        self.records.update(record_uids)
+        self.records.update(record_ids)
 
 
 class SoxData:
-    def __init__(self, ec_private_key, storage):    # type: (bytes, sox_storage.SqliteSoxStorage) -> None
+    def __init__(self, ec_private_key, storage):    # type: (bytes, sqlite_storage.SqliteSoxStorage) -> None
         self.ec_private_key = ec_private_key    # type: bytes
-        self.storage = storage                  # type: sox_storage.SqliteSoxStorage
+        self.storage = storage                  # type: sqlite_storage.SqliteSoxStorage
         self._records = {}                      # type: Dict[str, sox_types.Record]
-        self._users = {}                        # type: Dict[str, sox_types.EnterpriseUser]
+        self._users = {}                        # type: Dict[int, sox_types.EnterpriseUser]
         task = RebuildTask(True)
         self.rebuild_data(task)
 
-    def get_records(self, record_uids=None):
-        return self._records if record_uids is None else {uid: self._records.get(uid) for uid in record_uids}
+    def get_records(self, record_ids=None):
+        return self._records if record_ids is None else {uid: self._records.get(uid) for uid in record_ids}
 
-    def get_user(self, uid): # type: (str) -> sox_types.EnterpriseUser
+    def get_user(self, uid):    # type: (int) -> sox_types.EnterpriseUser
         return self._users.get(uid)
 
-    def get_users(self, user_uids=None):
-        return self._users if user_uids is None else {uid: self.get_user(uid) for uid in user_uids}
+    def get_users(self, user_ids=None):
+        return self._users if user_ids is None else {uid: self.get_user(uid) for uid in user_ids}
 
-    def get_user_records(self, user_uids=None):
-        users = self._users.values() if user_uids is None else {self.get_user(uid) for uid in user_uids}
-        recs = set()
+    def get_user_records(self, user_ids=None):  # type: (List[int]) -> List[sox_types.UserRecord]
+        users = self._users.values() if user_ids is None else {self.get_user(uid) for uid in user_ids}
+        user_records = []
         for user in users:
             for r_uid in user.records:
-                recs.add(sox_types.UserRecord(user.user_uid, self._records.get(r_uid)))
-        return recs
+                user_records.append(sox_types.UserRecord(user.user_uid, self._records.get(r_uid)))
+        return user_records
 
     @property
     def record_count(self):   # type: () -> int
         return len(self._records)
 
     def rebuild_data(self, changes):   # type: (RebuildTask) -> None
-        def load_records(store, records_uids=None):
-            # type: (sqlite_storage.SqliteSoxStorage) -> Dict[str, sox_types.Record]
-            if records_uids:
-                recs = set()
-                for ruid in records_uids:
-                    recs.update({rec for rec in store.records.select_by_filter('record_uid', ruid)})
-            else:
-                recs = {rec for rec in store.records.get_all()}
-            records = {sox_types.Record.load(rec, self.ec_private_key) for rec in recs}
+        def load_records(store, records_ids=None):
+            # type: (sqlite_storage.SqliteSoxStorage, Optional[Iterable[str]]) -> Dict[str, sox_types.Record]
+            record_entities = store.records.get_entities(records_ids) if records_ids else store.records.get_all()
+            records = [sox_types.Record.load(entity, self.ec_private_key) for entity in record_entities]
             return {record.record_uid: record for record in records}
 
         def link_user_records(store, users):
-            # type: (sqlite_storage.SqliteSoxStorage, Set[sox_types.EnterpriseUser]) -> None
+            # type: (sqlite_storage.SqliteSoxStorage, Iterable[sox_types.EnterpriseUser]) -> None
             links = store.get_user_record_links().get_all_links()
             for link in links:
                 for user in users:
                     if link.user_uid == user.user_uid:
                         user.records.append(link.record_uid)
 
-        def load_users(store):  # type: (sqlite_storage.SqliteSoxStorage) -> Dict[str, sox_types.EnterpriseUser]
-            users = {sox_types.EnterpriseUser.load(eu) for eu in store.users.get_all()}
+        def load_users(store):  # type: (sqlite_storage.SqliteSoxStorage) -> Dict[int, sox_types.EnterpriseUser]
+            users = [sox_types.EnterpriseUser.load(eu) for eu in store.users.get_all()]
             link_user_records(store, users)
             u_lookup = {user.user_uid: user for user in users}
             return u_lookup
 
         self._records.update(load_records(self.storage, changes.records))
-        self._users.update(load_users(self.storage))
+        if changes.is_full_sync:
+            self._users.update(load_users(self.storage))
