2035d9978ed6c73250ce6ff716f7a4664950bbc1
Sergey Kolupaev <skolupaev@keepersecurity.com>
2022-09-15 16:25:01 -0700

    Diceware password generator. KC-433

--- keepercommander/generator.py
+++ keepercommander/generator.py
@@ -7,15 +7,17 @@
 # Keeper Commander 
 # Contact: ops@keepersecurity.com
 #
+import abc
 import logging
 import os
 import random
 import string
-from typing import Optional
+from typing import Optional, List
 from secrets import choice
 
 from Cryptodome.Random.random import shuffle
 
+from . import crypto
 
 DEFAULT_PASSWORD_LENGTH = 32
 PW_SPECIAL_CHARACTERS = '!@#$%()+;<>=?[]{}^.,'
@@ -92,7 +94,13 @@ def generate(length=64):
     return rules(increment, increment, increment, lastincrement)
 
 
-class KeeperPasswordGenerator:
+class PasswordGenerator(abc.ABC):
+    @abc.abstractmethod
+    def generate(self):   # type: () -> str
+        pass
+
+
+class KeeperPasswordGenerator(PasswordGenerator):
     def __init__(self, length: int = DEFAULT_PASSWORD_LENGTH, symbols: Optional[int] = None,
                  digits: Optional[int] = None, caps: Optional[int] = None, lower: Optional[int] = None,
                  special_characters: str = PW_SPECIAL_CHARACTERS):
@@ -146,3 +154,29 @@ class KeeperPasswordGenerator:
                 length=length, caps=upper, lower=lower, digits=digits, symbols=symbols,
                 special_characters=special_characters
             )
+
+
+class DicewarePasswordGenerator(PasswordGenerator):
+    def __init__(self, number_of_rolls):   # type: (int) -> None
+        self._number_of_rolls = number_of_rolls if number_of_rolls> 0 else 5
+        dice_path = os.path.join(os.path.dirname(__file__), 'resources', 'diceware.wordlist.asc.txt')
+        self._vocabulary = None    # type: Optional[List[str]]
+        if os.path.isfile(dice_path):
+
+            with open(dice_path, 'r') as dw:
+                self._vocabulary = [x.split()[1].strip() for x in dw.readlines() if x]
+
+    def generate(self):
+        if not self._vocabulary:
+            raise Exception(f'Diceware word list was not loaded')
+
+        number_of_bytes = 2
+        random_bytes = crypto.get_random_bytes(self._number_of_rolls * number_of_bytes)
+        words = []
+        for i in range(self._number_of_rolls):
+            offset = i * number_of_bytes
+            rb = random_bytes[offset:offset+number_of_bytes]
+            rn = int.from_bytes(rb, byteorder='big', signed=False)
+            words.append(self._vocabulary[rn % len(self._vocabulary)])
+
+        return ' '.join(words)
