859e5f16c80fa738fa10af90d3bf13e20b5eb3cc
Sergey Kolupaev <sergey@keepersecurity.com>
2023-02-13 12:37:16 -0800

    Debug Flag for CLI-level App Call Fails to Set Debug Mode. KC-484

--- keepercommander/cli.py
+++ keepercommander/cli.py
@@ -40,7 +40,6 @@ from .recordv3 import init_recordv3_commands
 from .subfolder import BaseFolderNode
 
 stack = []
-command_info = OrderedDict()
 register_commands(commands, aliases, command_info)
 enterprise_command_info = OrderedDict()
 msp_command_info = OrderedDict()
@@ -452,7 +451,8 @@ def loop(params):  # type: (KeeperParams) -> int
         versioning.welcome_print_version(params)
 
     else:
-        logging.getLogger().setLevel(logging.WARNING)
+
+        logging.getLogger().setLevel(logging.DEBUG if params.debug else logging.WARNING)
 
     if params.user:
         try:
