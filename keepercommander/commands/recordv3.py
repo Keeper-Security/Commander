284138effda021ed9d78b974304a647ef15e502d
Sergey Kolupaev <sergey@keepersecurity.com>
2023-03-01 14:07:00 -0800

    Refactor 'totp' command

--- keepercommander/commands/recordv3.py
+++ keepercommander/commands/recordv3.py
@@ -15,15 +15,11 @@ import logging
 import os
 import re
 import tempfile
-import threading
 from pathlib import Path
-from typing import List
 
 import requests
 from Cryptodome.Cipher import AES
-from tabulate import tabulate
 
-from . import record_common
 from . import recordv2 as recordv2
 from .base import suppress_exit, raise_parse_exception, dump_report_data, Command
 from .register import FileReportCommand
@@ -31,10 +27,9 @@ from .. import api, crypto, generator
 from .. import recordv3, loginv3
 from ..display import bcolors
 from ..error import CommandError
-from ..params import KeeperParams, LAST_RECORD_UID
+from ..params import LAST_RECORD_UID
 from ..proto import record_pb2 as records
-from ..record import get_totp_code
-from ..subfolder import BaseFolderNode, find_folders, try_resolve_path, get_folder_path
+from ..subfolder import BaseFolderNode, try_resolve_path
 
 DEFAULT_GENERATE_PASSWORD_LENGTH = 16
 
@@ -42,7 +37,6 @@ DEFAULT_GENERATE_PASSWORD_LENGTH = 16
 def register_commands(commands):
     commands['add'] = RecordAddCommand()
     commands['edit'] = RecordEditCommand()
-    commands['totp'] = TotpCommand()
     commands['record-type-info'] = RecordTypeInfo()
     commands['record-type'] = RecordRecordType()
     commands['file-report'] = RecordFileReportCommand()
@@ -53,7 +47,7 @@ def register_command_info(aliases, command_info):
     aliases['rti'] = 'record-type-info'
     aliases['rt'] = 'record-type'
 
-    for p in [totp_parser, record_type_info_parser, record_type_parser, file_report_parser]:
+    for p in [record_type_info_parser, record_type_parser, file_report_parser]:
         command_info[p.prog] = p.description
 
 
@@ -114,14 +108,6 @@ edit_parser.add_argument('option', nargs='*', type=str, action='store', help='lo
 edit_parser.add_argument('-r', '--record', dest='record', required=True, type=str, action='store', help='record path or UID')
 edit_parser.error = raise_parse_exception
 edit_parser.exit = suppress_exit
-
-
-totp_parser = argparse.ArgumentParser(prog='totp', description='Display the Two Factor Code for a record')
-totp_parser.add_argument('record', nargs='?', type=str, action='store', help='record path or UID')
-totp_parser.add_argument('--legacy', dest='legacy', action='store_true', help='work with legacy records only')
-totp_parser.add_argument('--details', dest='details', action='store_true', help='display 2FA details')
-totp_parser.error = raise_parse_exception
-totp_parser.exit = suppress_exit
 
 
 record_type_info_parser = argparse.ArgumentParser(prog='record-type-info', description='Get record type info')
@@ -775,142 +761,6 @@ def delete_orphaned_attachments(params):
                         logging.warning('Delete attachment error: %s', status.get('status'))
 
             api.sync_down(params)
-
-
-class TotpEndpoint:
-    def __init__(self, record_uid, record_title, paths):
-        self.record_uid = record_uid
-        self.record_title = record_title
-        self.paths = paths
-
-
-class TotpCommand(Command):
-    LastRevision = 0  # int
-    Endpoints = []    # type: List[TotpEndpoint]
-
-    def get_parser(self):
-        return totp_parser
-
-    def execute(self, params, **kwargs):
-        record_name = kwargs['record'] if 'record' in kwargs else None
-        record_uid = None
-        if record_name:
-            if record_name in params.record_cache:
-                record_uid = record_name
-            else:
-                rs = try_resolve_path(params, record_name)
-                if rs is not None:
-                    folder, record_name = rs
-                    if folder is not None and record_name is not None:
-                        folder_uid = folder.uid or ''
-                        if folder_uid in params.subfolder_record_cache:
-                            for uid in params.subfolder_record_cache[folder_uid]:
-                                r = api.get_record(params, uid)
-                                if r.title.lower() == record_name.lower():
-                                    record_uid = uid
-                                    break
-
-            if record_uid is None:
-                records = api.search_records(params, kwargs['record'])
-                if len(records) == 1:
-                    logging.info('Record Title: {0}'.format(records[0].title))
-                    record_uid = records[0].record_uid
-                else:
-                    if len(records) == 0:
-                        raise CommandError('totp', 'Enter name or uid of existing record')
-                    else:
-                        raise CommandError('totp', 'More than one record are found for search criteria: {0}'.format(kwargs['record']))
-
-        if record_uid:
-            record = params.record_cache[record_uid] if record_uid in params.record_cache else {}
-            version = record.get('version') or 0
-            rec = api.get_record(params, record_uid)
-            if version == 3:
-                recordv3.RecordV3.validate_access(params, record_uid)
-                data = record.get('data_unencrypted') or '{}'
-                data = json.loads(data)
-                fields = data['fields'] if 'fields' in data else []
-                fields.extend(data['custom'] if 'custom' in data else [])
-                totp = next((t.get('value') for t in fields if t['type'] == 'oneTimeCode'), None)
-                rec.totp = totp[0] if totp else totp
-            if not rec.totp:
-                raise CommandError('totp', f'Record \"{rec.title}\" does not contain TOTP codes')
-
-            if kwargs['details']:
-                record_common.display_totp_details(rec.totp)
-
-
-            tmer = None     # type: threading.Timer or None
-            done = False
-            def print_code():
-                global tmer
-                if not done:
-                    TotpCommand.display_code(rec.totp)
-                    tmer = threading.Timer(1, print_code).start()
-
-            try:
-                print('Press <Enter> to exit\n')
-                print_code()
-                input()
-            finally:
-                done = True
-                if tmer:
-                    tmer.cancel()
-        else:
-            TotpCommand.find_endpoints(params)
-            logging.info('')
-            headers = ["#", 'Record UID', 'Record Title', 'Folder(s)']
-            table = []
-            for i in range(len(TotpCommand.Endpoints)):
-                endpoint = TotpCommand.Endpoints[i]
-                title = endpoint.record_title
-                if len(title) > 23:
-                    title = title[:20] + '...'
-                folder = endpoint.paths[0] if len(endpoint.paths) > 0 else '/'
-                table.append([i + 1, endpoint.record_uid, title, folder])
-            table.sort(key=lambda x: x[2])
-            print(tabulate(table, headers=headers))
-            print('')
-
-    LastDisplayedCode = ''
-
-    @staticmethod
-    def display_code(url):
-        code, remains, total = get_totp_code(url)
-        progress = ''.rjust(remains, '=')
-        progress = progress.ljust(total, ' ')
-        if os.isatty(0):
-            print('\r', end='', flush=True)
-            print('\t{0}\t\t[{1}]'.format(code, progress), end='', flush=True)
-        else:
-            if TotpCommand.LastDisplayedCode != code:
-                print('\t{0}\t\tvalid for {1} seconds.'.format(code, remains))
-                TotpCommand.LastDisplayedCode = code
-
-    @staticmethod
-    def find_endpoints(params):
-        # type: (KeeperParams) -> None
-        if TotpCommand.LastRevision < params.revision:
-            TotpCommand.LastRevision = params.revision
-            TotpCommand.Endpoints.clear()
-            for record_uid in params.record_cache:
-                rec = params.record_cache[record_uid]
-                version = rec.get('version')
-                record = api.get_record(params, record_uid)
-                if version == 3:
-                    data = rec.get('data_unencrypted') or '{}'
-                    data = json.loads(data)
-                    fields = data['fields'] if 'fields' in data else []
-                    fields.extend(data['custom'] if 'custom' in data else [])
-                    totp = next((t.get('value') for t in fields if t['type'] == 'oneTimeCode'), None)
-                    record.totp = totp[0] if totp else totp
-
-                if record.totp:
-                    paths = []
-                    for folder_uid in find_folders(params, record_uid):
-                        path = '/' + get_folder_path(params, folder_uid, '/')
-                        paths.append(path)
-                    TotpCommand.Endpoints.append(TotpEndpoint(record_uid, record.title, paths))
 
 
 get_record_types_description = '''
