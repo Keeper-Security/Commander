e3a21094c23d5607eafc647494bf83f4862e8ebc
Ayrris Aunario <aaunario@keepersecurity.com>
2023-04-07 16:35:06 -0500

    folder/record path-resolution improvement/fix: modify/add helper functions that enable resolution of folder/record paths to all associated UIDs

--- keepercommander/commands/helpers/record.py
+++ keepercommander/commands/helpers/record.py
@@ -1,23 +1,25 @@
+from typing import Set, Optional
+
 from keepercommander import api
 from keepercommander.params import KeeperParams
 from keepercommander.subfolder import try_resolve_path
 
 
-# Get record UID given one of its identifiers: name (if current folder contains the record), path, or UID
-def get_record_uid(params, name):   # type: (KeeperParams, str) -> str or None
-    uid = None
+# Get record UID(s) given one of its identifiers: name (if current folder contains the record), path, or UID
+def get_record_uids(params, name):  # type: (KeeperParams, str) -> Set[Optional[str]]
+    uids = set()
     if name in params.record_cache:
-        uid = name
+        uids = [name]
     else:
-        rs = try_resolve_path(params, name)
-        if rs is not None:
-            folder, name = rs
-            if folder is not None and name is not None:
+        rs = try_resolve_path(params, name, find_all_matches=True)
+        # if rs is not None:
+        folders, name = rs
+        if folders and name is not None:
+            for folder in folders:
                 folder_uid = folder.uid or ''
                 if folder_uid in params.subfolder_record_cache:
                     for r_uid in params.subfolder_record_cache[folder_uid]:
                         r = api.get_record(params, r_uid)
                         if r.title.lower() == name.lower():
-                            uid = r_uid
-                            break
-    return uid
+                            uids.add(r_uid)
+    return uids
