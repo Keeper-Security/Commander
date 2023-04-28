2f3aa1dc2d77f3be3fb592faea8283dae1022992
Ayrris Aunario <aaunario@keepersecurity.com>
2023-04-28 15:18:05 -0500

    `aging-report`/compliance data improvements/fixes: 1) fix Last Password Change value to show most recent datetime 2) fix bug w/ preliminary data being overwritten by compliance data update 3) fix bugs w/ cached data update history 4) add description of Aging Report results in title 5) add 'shared' sort option 6) add support for excluding deleted records from report

--- keepercommander/sox/__init__.py
+++ keepercommander/sox/__init__.py
@@ -64,15 +64,17 @@ def get_prelim_data(params, enterprise_id=0, rebuild=False, min_updated=0, cache
     def sync_down(name_by_id, store):  # type: (Dict[int, str], sqlite_storage.SqliteSoxStorage) ->  None
         def to_storage_types(user_data, username_lookup):
             def to_record_entity(record):
-                entity = StorageRecord()
-                entity.record_uid_bytes = record.recordUid
-                entity.record_uid = utils.base64_url_encode(record.recordUid)
+                record_uid_bytes = record.recordUid
+                record_uid = utils.base64_url_encode(record_uid_bytes)
+                entity = store.get_records().get_entity(record_uid) or StorageRecord()
+                entity.record_uid_bytes = record_uid_bytes
+                entity.record_uid = record_uid
                 entity.encrypted_data = record.encryptedData
                 entity.shared = record.shared
                 return entity
 
             def to_user_entity(user, email_lookup):
-                entity = StorageUser()
+                entity = store.get_users().get_entity(user.enterpriseUserId) or StorageUser()
                 entity.status = user.status
                 entity.user_uid = user.enterpriseUserId
                 entity.email = email_lookup.get(entity.user_uid)
@@ -137,6 +139,7 @@ def get_prelim_data(params, enterprise_id=0, rebuild=False, min_updated=0, cache
     refresh_data = rebuild or not last_updated or min_updated > last_updated or only_shared_cached and not shared_only
     if refresh_data and not cache_only:
         user_lookup = {x['enterprise_user_id']: x['username'] for x in params.enterprise.get('users', [])}
+        storage.clear_non_aging_data()
         sync_down(user_lookup, storage)
         storage.set_shared_records_only(shared_only)
     return sox_data.SoxData(ec_private_key=key, storage=storage, no_cache=no_cache)
@@ -338,3 +341,15 @@ def get_compliance_data(params, node_id, enterprise_id=0, rebuild=False, min_upd
     rebuild_task = sox_data.RebuildTask(is_full_sync=False, load_compliance_data=True)
     sd.rebuild_data(rebuild_task, no_cache=no_cache)
     return sd
+
+
+def get_node_id(params, name):
+    name = int(name) if isinstance(name, str) and name.isdecimal() else name
+    nodes = params.enterprise['nodes']
+    root_node_id = nodes[0].get('node_id', 0)
+    node_ids = (n.get('node_id') for n in nodes)
+    node_id_lookup = {n.get('data').get('displayname'): n.get('node_id') for n in nodes}
+    node_id = node_id_lookup.get(name) if name in node_id_lookup \
+        else name if name in node_ids \
+        else root_node_id
+    return node_id
