800929cc7bc97d52d4efb69a8415acd741a1b3c8
Sergey Kolupaev <sergey@keepersecurity.com>
2023-05-19 16:42:40 -0700

    LastPass import custom fields

--- keepercommander/importer/lastpass/account.py
+++ keepercommander/importer/lastpass/account.py
@@ -15,3 +15,12 @@ class Account(object):
         self.totp_url = totp_url
         self.last_modified = 0
         self.attachments = []
+        self.custom_fields = []
+
+
+class CustomField(object):
+    def __init__(self, f_name, f_type, f_value, checked):
+        self.name = f_name
+        self.type = f_type
+        self.value = f_value
+        self.checked = checked
