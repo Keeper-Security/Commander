2520cdb6ac99cb71d7be2a2fbaf6cc84a1025a13
Sergey Kolupaev <sergey@keepersecurity.com>
2023-08-06 12:51:03 -0700

    Thycotic archive script

--- examples/thycotic_archive.py
+++ examples/thycotic_archive.py
@@ -9,7 +9,6 @@ from typing import Dict, List, Tuple
 
 ATTACHMENT_FOLDER = 'Attachments'
 CSV_FILENAME = 'secrets.csv'
-column_B_index = 1
 ZIP_FILENAME = 'keeper-archive.zip'
 KEEPER_IMPORT = 'keeper-import.json'
 
@@ -35,17 +34,33 @@ logging.info('Loaded %d attachments from %s', len(attachment_files), ATTACHMENT_
 # load attachment.csv
 CSV_FILENAME = 'secrets.csv'
 secrets = []  # type: List[Tuple[str, int]]
+folders = {}  # type: Dict[int, str]
 with open(CSV_FILENAME, "r", encoding='utf-8-sig') as csvfile:
     reader = csv.reader(csvfile)
     next(csvfile)      # skip header row
     for row in reader:
-        if row and len(row) >= column_B_index + 4:
-            record_title = row[column_B_index]
-            secret_id = row[column_B_index + 2]
+        if row and len(row) >= 5:
+            record_title = row[1]
+            secret_id = row[3]
             if not secret_id.isnumeric():
                 logging.warning('CSV: record "%s" secret id is not integer', record_title)
                 continue
             secrets.append((record_title, int(secret_id)))
+
+            folder_name = row[0]
+            folder_id = row[4]
+            if folder_id.isnumeric():
+                folder_id = int(folder_id)
+                if folder_id > 0:
+                    if folder_id not in folders:
+                        if folder_name.startswith('\\'):
+                            folder_name = folder_name[1:]
+                        if not folder_name.endswith('\\'):
+                            folder_name += '\\'
+                        folders[folder_id] = folder_name
+            elif folder_id == '-1':
+                if folder_id not in folders:
+                    folders[-1] = '\\'
 
 if len(secrets) == 0:
     logging.error('No secrets were loaded from %s. Exiting', CSV_FILENAME)
@@ -66,12 +81,24 @@ logging.info('Loaded %d records from %s', len(import_json['records']), 'keeper-i
 duplicate_titles = set()
 secret_id_lookup = {x[1]: x[0] for x in secrets}    # type: Dict[int, str]
 record_title_lookup = {}     # type: Dict[str, dict]
+full_path_lookup = {}        # type: Dict[str, dict]
 for record in import_json['records']:
     title = record.get('title')
     if not title:
         logging.warning('Import record has no title. Skipping')
         continue
     title = title.casefold()
+    full_path = []
+    if 'folders' in record:
+        f = record['folders'][0]
+        if 'shared_folder' in f:
+            full_path.append(f['shared_folder'] or '')
+        if 'folder' in f:
+            full_path.append(f['folder'] or '')
+    full_path.append(title)
+    full_path = '\\'.join(x.casefold() for x in full_path if x)
+    full_path_lookup[full_path] = record
+
     if title in duplicate_titles:
         continue
     if title in record_title_lookup:
@@ -102,14 +129,23 @@ with zipfile.ZipFile(ZIP_FILENAME, mode='w', compresslevel=zipfile.ZIP_DEFLATED)
             continue
 
         title = secret_id_lookup[secret_id].casefold()
+        record = None
         if title in duplicate_titles:
-            skipped_attachments.append(full_path)
-            continue
-        if title not in record_title_lookup:
-            skipped_attachments.append(full_path)
+            if comps[0]:
+                folder_id = int(comps[0]) if comps[0].isnumeric() else -1
+                if folder_id in folders:
+                    full_record_path = folders[folder_id] + title
+                    full_record_path = full_record_path.casefold()
+                    if full_record_path in full_path_lookup:
+                        record = full_path_lookup[full_record_path]
+        else:
+            if title in record_title_lookup:
+                record = record_title_lookup[title]
+        if not record:
             logging.warning('Cannot file record with title "%s"', secret_id_lookup[secret_id])
+            skipped_attachments.append(full_path)
             continue
-        record = record_title_lookup[title]
+
         if 'attachments' not in record:
             record['attachments'] = []
         if os.path.isfile(full_path):
@@ -124,13 +160,13 @@ with zipfile.ZipFile(ZIP_FILENAME, mode='w', compresslevel=zipfile.ZIP_DEFLATED)
     f = json.dumps(import_json, indent=2, ensure_ascii=False)
     zf.writestr('export.json', f)
 
-if len(duplicate_titles) > 0:
-    logging.warning('\nSecret Duplicates')
-    no = 1
-    for title in duplicate_titles:
-        logging.warning(f'{no:>5} : {title}')
-        no += 1
-
+# if len(duplicate_titles) > 0:
+#     logging.warning('\nSecret Duplicates')
+#     no = 1
+#     for title in duplicate_titles:
+#         logging.warning(f'{no:>5} : {title}')
+#         no += 1
+#
 if len(skipped_attachments) > 0:
     logging.warning('\nSkipped attachments')
     no = 1
