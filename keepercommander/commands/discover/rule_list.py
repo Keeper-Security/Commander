4c9694f720d9d7fdc8c0d18327fc8ce4a949ecaf
John Walstra <jwalstra@keepersecurity.com>
2023-10-10 11:59:10 -0500

    DR-496 Add PAM Discovery
    Add to the pam action commands the ability to start a discovery job.

---
+++ keepercommander/commands/discover/rule_list.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+import argparse
+from . import PAMGatewayActionDiscoverCommandBase, GatewayContext
+from ...display import bcolors
+from ..pam.router_helper import router_get_connected_gateways
+from discovery_common.rule import Rules
+from discovery_common.types import RuleTypeEnum
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from discovery_common.types import RuleItem
+
+
+class PAMGatewayActionDiscoverRuleListCommand(PAMGatewayActionDiscoverCommandBase):
+    parser = argparse.ArgumentParser(prog='dr-discover-rule-list')
+    parser.add_argument('--gateway', '-g', required=True, dest='gateway', action='store',
+                        help='Gateway name of UID.')
+    parser.add_argument('--search', '-s', required=False, dest='search', action='store',
+                        help='Search for rules.')
+
+    def get_parser(self):
+        return PAMGatewayActionDiscoverRuleListCommand.parser
+
+    @staticmethod
+    def print_rule_table(rule_list: List[RuleItem]):
+
+        print("")
+        print(f"{bcolors.HEADER}{'Rule ID'.ljust(15, ' ')} "
+              f"{'Action'.ljust(6, ' ')} "
+              f"{'Priority'.ljust(8, ' ')} "
+              f"{'Case'.ljust(12, ' ')} "
+              f"{'Added'.ljust(19, ' ')} "
+              f"{'Shared Folder UID'.ljust(22, ' ')} "
+              "Rule"
+              f"{bcolors.ENDC}")
+
+        print(f"{''.ljust(15, '=')} "
+              f"{''.ljust(6, '=')} "
+              f"{''.ljust(8, '=')} "
+              f"{''.ljust(12, '=')} "
+              f"{''.ljust(19, '=')} "
+              f"{''.ljust(22, '=')} "
+              f"{''.ljust(10, '=')} ")
+
+        for rule in rule_list:
+            if rule.case_sensitive is True:
+                ignore_case_str = "Sensitive"
+            else:
+                ignore_case_str = "Insensitive"
+
+            shared_folder_uid = ""
+            if rule.shared_folder_uid is not None:
+                shared_folder_uid = rule.shared_folder_uid
+            print(f"{bcolors.OKGREEN}{rule.rule_id.ljust(14, ' ')}{bcolors.ENDC} "
+                  f"{rule.action.value.ljust(6, ' ')} "
+                  f"{str(rule.priority).rjust(8, ' ')} "
+                  f"{ignore_case_str.ljust(12, ' ')} "
+                  f"{rule.added_ts_str.ljust(19, ' ')} "
+                  f"{shared_folder_uid.ljust(22, ' ')} "
+                  f"{Rules.make_action_rule_statement_str(rule.statement)}")
+
+    def execute(self, params, **kwargs):
+
+        if not hasattr(params, 'pam_controllers'):
+            router_get_connected_gateways(params)
+
+        gateway = kwargs.get("gateway")
+        gateway_context = GatewayContext.from_gateway(params, gateway)
+        if gateway_context is None:
+            print(f'{bcolors.FAIL}Discovery job gateway [{gateway}] was not found.{bcolors.ENDC}')
+            return
+
+        rules = Rules(record=gateway_context.configuration, params=params)
+        rule_list = rules.rule_list(rule_type=RuleTypeEnum.ACTION,
+                                    search=kwargs.get("search"))  # type: List[RuleItem]
+        if len(rule_list) == 0:
+            print(f"{bcolors.FAIL}There are no rules. Use 'pam action discovery rule add' "
+                  f"to create rules.{bcolors.ENDC}")
+            return
+
+        self.print_rule_table(rule_list=rule_list)
---
+++ keepercommander/commands/discover/rule_update.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+import argparse
+from . import PAMGatewayActionDiscoverCommandBase, GatewayContext
+from .rule_add import PAMGatewayActionDiscoverRuleAddCommand
+from ..pam.router_helper import router_get_connected_gateways
+from ...display import bcolors
+from discovery_common.rule import Rules, RuleTypeEnum
+
+
+class PAMGatewayActionDiscoverRuleUpdateCommand(PAMGatewayActionDiscoverCommandBase):
+    parser = argparse.ArgumentParser(prog='dr-discover-rule-add')
+    parser.add_argument('--gateway', '-g', required=True, dest='gateway', action='store',
+                        help='Gateway name of UID.')
+    parser.add_argument('--rule-id', '-i', required=True, dest='rule_id', action='store',
+                        help='Identifier for the rule')
+    parser.add_argument('--action', '-a', required=False, choices=['add', 'ignore', 'prompt'],
+                        dest='rule_action', action='store', help='Update the action to take if rule matches')
+    parser.add_argument('--priority', '-p', required=False, dest='priority', action='store', type=int,
+                        help='Update the rule execute priority')
+    parser.add_argument('--ignore-case', required=False, dest='ignore_case', action='store_true',
+                        help='Update the rule to ignore case')
+    parser.add_argument('--no-ignore-case', required=False, dest='ignore_case', action='store_false',
+                        help='Update the rule to not ignore case')
+    parser.add_argument('--shared-folder-uid', required=False, dest='shared_folder_uid',
+                        action='store', help='Update the folder to place record.')
+    parser.add_argument('--statement', '-s', required=False, dest='statement', action='store',
+                        help='Update the rule statement')
+
+    def get_parser(self):
+        return PAMGatewayActionDiscoverRuleUpdateCommand.parser
+
+    def execute(self, params, **kwargs):
+
+        if not hasattr(params, 'pam_controllers'):
+            router_get_connected_gateways(params)
+
+        gateway = kwargs.get("gateway")
+        gateway_context = GatewayContext.from_gateway(params, gateway)
+        if gateway_context is None:
+            print(f'{bcolors.FAIL}Discovery job gateway [{gateway}] was not found.{bcolors.ENDC}')
+            return
+
+        try:
+            rule_id = kwargs.get("rule_id")
+            rules = Rules(record=gateway_context.configuration, params=params)
+            rule_item = rules.get_rule_item(rule_type=RuleTypeEnum.ACTION, rule_id=rule_id)
+            if rule_item is None:
+                raise ValueError("Rule Id does not exist.")
+
+            rule_action = kwargs.get("rule_action")
+            if rule_action is not None:
+                rule_item.action = RuleTypeEnum.find_enum(rule_action)
+            priority = kwargs.get("priority")
+            if priority is not None:
+                rule_item.priority = priority
+            ignore_case = kwargs.get("ignore_case")
+            if ignore_case is not None:
+                rule_item.case_sensitive = not ignore_case
+            shared_folder_uid = kwargs.get("shared_folder_uid")
+            if shared_folder_uid is not None:
+                rule_item.shared_folder_uid = shared_folder_uid
+            statement = kwargs.get("statement")
+            if statement is not None:
+                rule_item.statement = PAMGatewayActionDiscoverRuleAddCommand.validate_rule_statement(
+                    params=params,
+                    gateway_context=gateway_context,
+                    statement=statement
+                )
+            rules.update_rule(rule_item)
+            print(f"{bcolors.OKGREEN}Rule has been updated{bcolors.ENDC}")
+        except Exception as err:
+            print(f"{bcolors.FAIL}Rule was not updated: {err}{bcolors.ENDC}")
