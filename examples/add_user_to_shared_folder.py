230f28e9fb19bffdf262359b1e6298e406e0a2ea
Sergey Kolupaev <sergey@keepersecurity.com>
2024-01-30 09:00:51 -0800

    Retrieve audit events for shared folder

--- examples/add_user_to_shared_folder.py
+++ examples/add_user_to_shared_folder.py
@@ -4,14 +4,14 @@ import os
 import sys
 
 from keepercommander.__main__ import get_params_from_config
-from keepercommander import api, utils, crypto
+from keepercommander import api, utils, crypto, error
 from keepercommander.proto import folder_pb2
 
 parser = argparse.ArgumentParser(description='Add user to shared folder')
 parser.add_argument('--debug', action='store_true', help='Enables debug logging')
-parser.add_argument('-p', '--manage-records', dest='manage_records', action='store_true',
+parser.add_argument('-p', '--manage-records', dest='manage_records', action='store', choices=['on', 'off'],
                     help='account permission: can manage records.')
-parser.add_argument('-o', '--manage-users', dest='manage_users', action='store_true',
+parser.add_argument('-o', '--manage-users', dest='manage_users', action='store',  choices=['on', 'off'],
                     help='account permission: can manage users.')
 parser.add_argument('--add', dest='add', action='append', help='Users to add to a shared folder.')
 parser.add_argument('--remove', dest='remove', action='append', help='Users to remove from shared folder.')
@@ -20,7 +20,7 @@ opts, flags = parser.parse_known_args(sys.argv[1:])
 
 logging.basicConfig(level=logging.DEBUG if opts.debug is True else logging.WARNING, format='%(message)s')
 
-my_params = get_params_from_config(os.path.join(os.path.dirname(__file__), 'config.json'))
+my_params = get_params_from_config('')
 
 api.login(my_params)
 if not my_params.session_token:
@@ -39,13 +39,16 @@ sf_rq = {
 }
 
 sf_rs = api.communicate(my_params, sf_rq)
+if len(sf_rs['shared_folders']) == 0:
+    raise ValueError(f'Shared folder UID "{shared_folder_uid}" not found')
+
 shared_folder_info = sf_rs['shared_folders'][0]
 shared_folder_key = utils.base64_url_decode(shared_folder_info['shared_folder_key'])
 shared_folder_key = crypto.decrypt_aes_v1(shared_folder_key, my_params.data_key)
 
 existing_users = set()
-if isinstance(sf_rs.get('users'), list):
-    existing_users.update((x.get('email').lower() for x in sf_rs['users']))
+if isinstance(shared_folder_info.get('users'), list):
+    existing_users.update((x.get('username').lower() for x in shared_folder_info['users']))
 
 users_to_add = []
 if hasattr(opts, 'add'):
@@ -53,7 +56,7 @@ if hasattr(opts, 'add'):
         for email in opts.add:
             email = email.lower()
             if email in existing_users:
-                logging.info('Add user "%s": already belongs to the shared folder')
+                logging.warning('Add user "%s": already belongs to the shared folder', email)
             else:
                 users_to_add.append(email)
 
@@ -65,14 +68,18 @@ if hasattr(opts, 'remove'):
             if email in existing_users:
                 users_to_remove.append(email)
             else:
-                logging.info('Remove user "%s": does not belongs to the shared folder')
+                logging.warning('Remove user "%s": does not belongs to the shared folder', email)
 
 if len(users_to_add) > 0:
     public_keys = {x: None for x in users_to_add}
     api.load_user_public_keys(my_params, users_to_add, False)
 
-manage_users = opts.manage_users
-manage_records = opts.manage_records
+manage_users = None
+manage_records = None
+if opts.manage_users:
+    manage_users = opts.manage_users == 'on'
+if opts.manage_records:
+    manage_records = opts.manage_records == 'on'
 
 rq = folder_pb2.SharedFolderUpdateV3Request()
 rq.sharedFolderUid = utils.base64_url_decode(opts.shared_folder)
@@ -90,6 +97,8 @@ for user in users_to_add:
         user_rsa_key = crypto.load_rsa_public_key(public_keys.rsa)
         arq.sharedFolderKey = crypto.encrypt_rsa(shared_folder_key, user_rsa_key)
         rq.sharedFolderAddUser.append(arq)
+    else:
+        logging.warning('Add user "%s": User public key is not available', user)
 
 for user in users_to_remove:
     rq.sharedFolderRemoveUser.append(user)
@@ -97,7 +106,7 @@ for user in users_to_remove:
 rs = api.communicate_rest(my_params, rq, 'vault/shared_folder_update_v3', rs_type=folder_pb2.SharedFolderUpdateV3Response)
 for add_status in rs.sharedFolderAddUserStatus:
     if add_status.status != 'success':
-        logging.info(f'Failed to add user {add_status.username} to shared folder: {add_status.status}')
+        logging.warning(f'Failed to add user {add_status.username} to shared folder: {add_status.status}')
 for remove_status in rs.sharedFolderRemoveUserStatus:
     if remove_status.status != 'success':
-        logging.info(f'Failed to add user {remove_status.username} to shared folder: {remove_status.status}')
+        logging.warning(f'Failed to add user {remove_status.username} to shared folder: {remove_status.status}')
