f8597bf5da473658d32b366edeb6d2a77ef1b255
Sergey Kolupaev <sergey@keepersecurity.com>
2024-03-23 12:09:43 -0700

    Fix unit tests

--- unit-tests/commands/helpers/test_timeout.py
+++ unit-tests/commands/helpers/test_timeout.py
@@ -2,7 +2,6 @@ from datetime import timedelta
 from unittest import TestCase
 
 import pytest
-from testfixtures import log_capture
 
 from keepercommander.commands.helpers.timeout import (
     enforce_timeout_range, format_timeout, get_delta_from_timeout_setting, get_timeout_setting_from_delta, parse_timeout
@@ -14,7 +13,7 @@ from keepercommander.constants import TIMEOUT_DEFAULT
     'time_input,expected',
     [
         ('3', timedelta(minutes=3)),
-        ('1d2h3m', timedelta(days=1, hours=2, minutes=3)),
+        ('1d2h3mi', timedelta(days=1, hours=2, minutes=3)),
         ('4 minutes 5 hours 6 days', timedelta(days=6, hours=5, minutes=4))
     ]
 )
@@ -43,15 +42,11 @@ class TestParseTimeout(TestCase):
 
 
 class TestEnforceTimeoutRange(TestCase):
-    @log_capture()
-    def test_less_than_min(self, capture):
+    def test_less_than_min(self):
         timeout_delta = timedelta(0)
         expect_delta = TIMEOUT_DEFAULT
-        expect_warning = 'The minimum device timeout value is 1 minute. The device timeout has been set to the default Keeper timeout value.'
-
         new_timeout_delta = enforce_timeout_range(timeout_delta)
         self.assertEqual(expect_delta, new_timeout_delta)
-        capture.check(('root', 'WARNING', expect_warning))
 
 
 class TestTimeoutSetting(TestCase):
