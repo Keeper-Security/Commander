e7bf99d7fc5494cb39eac9420b85b4d20f580f62
Ayrris Aunario <aaunario@keepersecurity.com>
2025-03-17 13:33:30 -0500

    `share-record --contacts-only` fix, compliance-report regex-filter optimization

--- keepercommander/commands/helpers/reporting.py
+++ keepercommander/commands/helpers/reporting.py
@@ -5,23 +5,16 @@ def filter_rows(rows, patterns, use_regex=False):
     if not patterns:
         return rows
 
-    pattern = re.compile('|'.join(patterns))
     is_a = lambda o, t: isinstance(o, t)
-    to_string = lambda el: el if is_a(el, str) else is_a(el, list) and ''.join([to_string(c) for c in el]) or str(el) or ''
-    match_regex = lambda r: bool(pattern.search(r))
-
-    def match_simple(r):
-        r = r.casefold()
-        for p in patterns:
-            if p.casefold() in r:
-                return True
-        return False
-
-    is_match = lambda r: use_regex and match_regex(r) or match_simple(r)
-
+    to_string = lambda el: el if is_a(el, str) else is_a(el, list) and ' '.join([to_string(c) for c in el]) or str(el) or ''
+    if use_regex:
+        pattern = re.compile('|'.join(patterns))
+        is_match = lambda row: bool(pattern.search(row))
+    else:
+        is_match = lambda row: any(p for p in patterns if p.casefold() in row.casefold())
     return list(
         filter(
-            lambda r: is_match(to_string(r)),
+            lambda row: is_match(to_string(row)),
             rows
         )
     )
