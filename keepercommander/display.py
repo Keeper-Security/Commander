60ebc611c479f976df9a5cacd830799d6ae6d85b
anantm-metron <anant.mangalampalli@metronlabs.com>
2025-06-24 10:42:11 +0530

    Update 'warning' color for better readability

--- keepercommander/display.py
+++ keepercommander/display.py
@@ -26,7 +26,7 @@ class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
+    WARNING = '\033[33m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
