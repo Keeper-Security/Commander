49611fa38b6e3eb84ae1001d77f250e597d22368
amangalampalli-ks <amangalampalli@keepersecurity.com>
2025-10-15 12:16:48 +0530

    Fix unit-tests in github

--- keepercommander/qrc/__init__.py
+++ keepercommander/qrc/__init__.py
@@ -1,13 +1,16 @@
-from setuptools import Extension
 import os
 
 ext_modules = []
 qrc_c_source = "keepercommander/qrc/mlkem/fastmathmodule.c"
 
-if os.path.exists(qrc_c_source):
-    ext_modules.append(Extension(
-        "keepercommander.qrc.mlkem.fastmath",
-        sources=[qrc_c_source],
-        extra_compile_args=["-std=c99"],
-        optional=True
-    ))
+try:
+    from setuptools import Extension
+    if os.path.exists(qrc_c_source):
+        ext_modules.append(Extension(
+            "keepercommander.qrc.mlkem.fastmath",
+            sources=[qrc_c_source],
+            extra_compile_args=["-std=c99"],
+            optional=True
+        ))
+except ImportError:
+    pass
