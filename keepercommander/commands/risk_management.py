8488aabb34096229c0ab44db209426086aa8fa61
Roman Vasilyev <rvasilyev@keepersecurity.com>
2025-10-31 12:24:38 -0700

    risk management enterprise and security stats

--- keepercommander/commands/risk_management.py
+++ keepercommander/commands/risk_management.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import json
 
 from . import base, enterprise_common, audit_alerts
 from .. import api
@@ -11,11 +12,27 @@ class RiskManagementReportCommand(base.GroupCommand):
         super().__init__()
         self.register_command('user', RiskManagementUserReportCommand(), 'Show Risk Management User report', 'u')
         self.register_command('alert', RiskManagementAlertReportCommand(), 'Show Risk Management Alert report', 'a')
+        self.register_command('enterprise-stat', RiskManagementEnterpriseStatCommand(), 'Show Risk Management recent login count', 'es')
+        self.register_command('enterprise-stat-details', RiskManagementEnterpriseStatDetailsCommand(), 'Gets the recent login count (users who logged in the last 30 days) '
+                              'and the number of users who have at least one record in their Vault', 'esd')
+        self.register_command('security-alerts-summary', RiskManagementSecurityAlertsSummaryCommand(), 'Gets the summary of events that happened in the last 30 days with '
+                              'a comparison to the previous 30 days.', 'sas')
+        self.register_command('security-alerts-detail', RiskManagementSecurityAlertDetailCommand(), 'Gets the details of event that happened in the last 30 days with a '
+                              'comparison to the previous 30 days. The response is paginated with a page size of 10000 users.', 'sad')
 
 
 rmd_user_parser = argparse.ArgumentParser(prog='risk-management user', description='Risk management user report', parents=[base.report_output_parser])
 
 rmd_alert_parser = argparse.ArgumentParser(prog='risk-management alert', description='Risk management alert report', parents=[base.report_output_parser])
+
+rmd_enterprise_stat_detail_parser = argparse.ArgumentParser(prog='risk-management enterprise-stat-details', description='Risk management enterprise stat details', parents=[base.report_output_parser])
+
+rmd_security_alerts_summary_parser = argparse.ArgumentParser(prog='risk-management security-alerts-summary', description='Risk management security alerts summary', parents=[base.report_output_parser])
+
+rmd_security_alerts_detail_parser = argparse.ArgumentParser(prog='risk-management security-alerts-detail', description='Risk management security alerts detail', parents=[base.report_output_parser])
+rmd_security_alerts_detail_parser.add_argument(
+    '--aetid', dest='aetid', type=int, action='store',
+    help='show the details for audit event type ID.')
 
 class RiskManagementUserReportCommand(enterprise_common.EnterpriseCommand):
     def get_parser(self):
@@ -114,3 +131,100 @@ class RiskManagementAlertReportCommand(enterprise_common.EnterpriseCommand):
             header = [base.field_to_title(x) for x in header]
 
         return base.dump_report_data(rows, headers=header, fmt=kwargs.get('format'), filename=kwargs.get('output'))
+
+
+class RiskManagementEnterpriseStatCommand(enterprise_common.EnterpriseCommand):
+    def execute(self, params, **kwargs):
+        rs = api.communicate_rest(params, None, 'rmd/get_enterprise_stat', rs_type=rmd_pb2.EnterpriseStat)
+        print('{0:>20s}:'.format('Users Enterprise Stat'))
+        print('{0:>20s}: {1:<20d}'.format('Logged in', rs.usersLoggedRecent))
+        print('{0:>20s}: {1:<20d}'.format('Has records', rs.usersHasRecords))
+
+
+class RiskManagementEnterpriseStatDetailsCommand(enterprise_common.EnterpriseCommand):
+    def get_parser(self):
+        return rmd_enterprise_stat_detail_parser
+
+    def execute(self, params, **kwargs):
+        request = rmd_pb2.EnterpriseStatDetailsRequest()
+        done = False
+        header = [
+                'enterprise_user_id',
+                'last_logged_in',
+                'has_records',
+                ]
+        out_format = kwargs.get('format')
+        if out_format != 'json':
+            header = [base.field_to_title(x) for x in header]
+        rows = []
+        while not done:
+            response = api.communicate_rest(params, request, 'rmd/get_enterprise_stat_details', rs_type=rmd_pb2.EnterpriseStatDetailsResponse)
+            done = not response.hasMore
+            request.continuationToken.lastUpdated = response.continuationToken.lastUpdated
+            request.continuationToken.enterpriseUserId = response.continuationToken.enterpriseUserId
+            for esd in response.enterpriseStatDetails:
+                rows.append([esd.enterpriseUserId, esd.lastLoggedIn, esd.hasRecords])
+        return base.dump_report_data(rows, headers=header, fmt=out_format, filename=kwargs.get('output'))
+
+
+class RiskManagementSecurityAlertsSummaryCommand(enterprise_common.EnterpriseCommand):
+    def get_parser(self):
+        return rmd_security_alerts_summary_parser
+
+    def execute(self, params, **kwargs):
+        header = [
+                'audit_event_type_id',
+                'current_count',
+                'current_user_count',
+                'previous_count',
+                'previous_user_count',
+                ]
+        out_format = kwargs.get('format')
+        if out_format != 'json':
+            header = [base.field_to_title(x) for x in header]
+        rows = []
+        response = api.communicate_rest(params, None, 'rmd/get_security_alerts_summary', rs_type=rmd_pb2.SecurityAlertsSummaryResponse)
+        for node in response.securityAlertsSummary:
+            rows.append([
+                node.auditEventTypeId,
+                node.currentCount,
+                node.currentUserCount,
+                node.previousCount,
+                node.previousUserCount,
+                ])
+        return base.dump_report_data(rows, headers=header, fmt=out_format, filename=kwargs.get('output'))
+
+
+class RiskManagementSecurityAlertDetailCommand(enterprise_common.EnterpriseCommand):
+    def get_parser(self):
+        return rmd_security_alerts_detail_parser
+
+    def execute(self, params, **kwargs):
+        request = rmd_pb2.SecurityAlertsDetailRequest()
+        aetid = kwargs.get('aetid') or 0
+        if aetid < 1:
+            raise ValueError(f'Invalid aetid {aetid}: valid aetid > 0')
+        request.auditEventTypeId = aetid
+        done = False
+        header = [
+                'enterprise_user_id',
+                'current_count',
+                'previous_count',
+                'last_occurrence',
+                ]
+        out_format = kwargs.get('format')
+        if out_format != 'json':
+            header = [base.field_to_title(x) for x in header]
+        rows = []
+        while not done:
+            response = api.communicate_rest(params, request, 'rmd/get_security_alerts_detail', rs_type=rmd_pb2.SecurityAlertsDetailResponse)
+            done = not response.hasMore
+            request.continuationToken = response.continuationToken
+            for node in response.securityAlertDetails:
+                rows.append([
+                    node.enterpriseUserId,
+                    node.currentCount,
+                    node.previousCount,
+                    node.lastOccurrence,
+                    ])
+        return base.dump_report_data(rows, headers=header, fmt=out_format, filename=kwargs.get('output'))
