ba5496d545a67ec6136183ab3927e6f3bd19a3bf
Sergey Kolupaev <skolupaev@keepersecurity.com>
2026-02-20 10:10:36 -0800

    Update proto files

--- keepercommander/proto/MCTransfer_pb2.pyi
+++ keepercommander/proto/MCTransfer_pb2.pyi
@@ -9,17 +9,19 @@ DESCRIPTOR: _descriptor.FileDescriptor
 class MCTransferStatus(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = []
     STATUS_INVALID: _ClassVar[MCTransferStatus]
-    STATUS_INITIATED: _ClassVar[MCTransferStatus]
+    STATUS_REQUESTED: _ClassVar[MCTransferStatus]
     STATUS_ACCEPTED: _ClassVar[MCTransferStatus]
     STATUS_PENDING_APPROVAL: _ClassVar[MCTransferStatus]
     STATUS_APPROVED: _ClassVar[MCTransferStatus]
     STATUS_DENIED: _ClassVar[MCTransferStatus]
+    STATUS_READY: _ClassVar[MCTransferStatus]
 STATUS_INVALID: MCTransferStatus
-STATUS_INITIATED: MCTransferStatus
+STATUS_REQUESTED: MCTransferStatus
 STATUS_ACCEPTED: MCTransferStatus
 STATUS_PENDING_APPROVAL: MCTransferStatus
 STATUS_APPROVED: MCTransferStatus
 STATUS_DENIED: MCTransferStatus
+STATUS_READY: MCTransferStatus
 
 class MCTransferRequest(_message.Message):
     __slots__ = ["enterpriseName", "enterpriseAdminEmail", "mcTransferTreeKeys"]
@@ -40,12 +42,14 @@ class MCTransferTreeKey(_message.Message):
     def __init__(self, enterpriseId: _Optional[int] = ..., treeKey: _Optional[bytes] = ...) -> None: ...
 
 class MCTransferApproveDenyRequest(_message.Message):
-    __slots__ = ["enterpriseId", "message"]
+    __slots__ = ["enterpriseId", "message", "transferDate"]
     ENTERPRISEID_FIELD_NUMBER: _ClassVar[int]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    TRANSFERDATE_FIELD_NUMBER: _ClassVar[int]
     enterpriseId: int
     message: str
-    def __init__(self, enterpriseId: _Optional[int] = ..., message: _Optional[str] = ...) -> None: ...
+    transferDate: int
+    def __init__(self, enterpriseId: _Optional[int] = ..., message: _Optional[str] = ..., transferDate: _Optional[int] = ...) -> None: ...
 
 class MCTransferApproveDenyResponse(_message.Message):
     __slots__ = ["mcTransferState"]
@@ -68,7 +72,7 @@ class MCTransferEnterprise(_message.Message):
     def __init__(self, enterpriseId: _Optional[int] = ..., enterpriseName: _Optional[str] = ...) -> None: ...
 
 class MCTransferState(_message.Message):
-    __slots__ = ["movingEnterpriseId", "movingEnterpriseName", "movingEnterpriseAdminEmail", "receivingEnterpriseName", "receivingEnterpriseAdminEmail", "transferStatus", "comments", "mcTransferEnterprises"]
+    __slots__ = ["movingEnterpriseId", "movingEnterpriseName", "movingEnterpriseAdminEmail", "receivingEnterpriseName", "receivingEnterpriseAdminEmail", "transferStatus", "comments", "transferDate", "mcTransferEnterprises"]
     MOVINGENTERPRISEID_FIELD_NUMBER: _ClassVar[int]
     MOVINGENTERPRISENAME_FIELD_NUMBER: _ClassVar[int]
     MOVINGENTERPRISEADMINEMAIL_FIELD_NUMBER: _ClassVar[int]
@@ -76,6 +80,7 @@ class MCTransferState(_message.Message):
     RECEIVINGENTERPRISEADMINEMAIL_FIELD_NUMBER: _ClassVar[int]
     TRANSFERSTATUS_FIELD_NUMBER: _ClassVar[int]
     COMMENTS_FIELD_NUMBER: _ClassVar[int]
+    TRANSFERDATE_FIELD_NUMBER: _ClassVar[int]
     MCTRANSFERENTERPRISES_FIELD_NUMBER: _ClassVar[int]
     movingEnterpriseId: int
     movingEnterpriseName: str
@@ -84,5 +89,6 @@ class MCTransferState(_message.Message):
     receivingEnterpriseAdminEmail: str
     transferStatus: MCTransferStatus
     comments: str
+    transferDate: int
     mcTransferEnterprises: _containers.RepeatedCompositeFieldContainer[MCTransferEnterprise]
-    def __init__(self, movingEnterpriseId: _Optional[int] = ..., movingEnterpriseName: _Optional[str] = ..., movingEnterpriseAdminEmail: _Optional[str] = ..., receivingEnterpriseName: _Optional[str] = ..., receivingEnterpriseAdminEmail: _Optional[str] = ..., transferStatus: _Optional[_Union[MCTransferStatus, str]] = ..., comments: _Optional[str] = ..., mcTransferEnterprises: _Optional[_Iterable[_Union[MCTransferEnterprise, _Mapping]]] = ...) -> None: ...
+    def __init__(self, movingEnterpriseId: _Optional[int] = ..., movingEnterpriseName: _Optional[str] = ..., movingEnterpriseAdminEmail: _Optional[str] = ..., receivingEnterpriseName: _Optional[str] = ..., receivingEnterpriseAdminEmail: _Optional[str] = ..., transferStatus: _Optional[_Union[MCTransferStatus, str]] = ..., comments: _Optional[str] = ..., transferDate: _Optional[int] = ..., mcTransferEnterprises: _Optional[_Iterable[_Union[MCTransferEnterprise, _Mapping]]] = ...) -> None: ...
