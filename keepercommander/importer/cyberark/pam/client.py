870012a68a07dc8eed4a692975400b882560af3e
pvagare-ks <pvagare@keepersecurity.com>
2026-08-19 11:53:21 +0530

    updated reason

--- keepercommander/importer/cyberark/pam/client.py
+++ keepercommander/importer/cyberark/pam/client.py
@@ -1206,7 +1206,7 @@ class CyberArkPVWAClient:
                     self._get_url("account_password").format(account_id=account_id),
                     headers={"Authorization": self.auth_token, "Content-Type": "application/json"},
                     json={
-                        "reason": "test",
+                        "reason": "Keeper Commander Import",
                         **({"TicketingSystemName": environ["KEEPER_CYBERARK_TICKETING_SYSTEM"]}
                            if "KEEPER_CYBERARK_TICKETING_SYSTEM" in environ else {}),
                         **({"TicketId": environ["KEEPER_CYBERARK_TICKET_ID"]}
