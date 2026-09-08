6e06eea420d758235515abf05ca395e3977a982c
Sergey Kolupaev <sergey@keepersecurity.com>
2026-09-08 14:16:08 -0700

    epm scim: support +group_name syntax for AD

--- tests/test_scim.py
+++ tests/test_scim.py
@@ -2,6 +2,23 @@ from unittest import TestCase
 
 import pytest
 from keepercommander.commands import scim
+from keepercommander.scim.data_sources import AdCrmDataSource
+
+
+def test_ad_group_selector_keeps_direct_membership_by_default():
+    group, expand_subgroups = AdCrmDataSource._parse_group_selector('Engineering')
+
+    assert group == 'Engineering'
+    assert expand_subgroups is False
+
+
+def test_ad_group_selector_expands_direct_subgroups_when_prefixed_with_plus():
+    group, expand_subgroups = AdCrmDataSource._parse_group_selector('+CN=Engineering,DC=example,DC=com')
+
+    assert group == 'CN=Engineering,DC=example,DC=com'
+    assert expand_subgroups is True
+    assert AdCrmDataSource._direct_subgroups_filter(group) == (
+        '(&(objectClass=group)(memberOf=CN=Engineering,DC=example,DC=com))')
 
 @pytest.mark.skip
 class TestScimCommands(TestCase):
